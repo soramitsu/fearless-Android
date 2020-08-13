f07d2d32bff141096cc7454eee378e18b107628f
Ali Abdulmadzhidov <mrfreezzzer@mail.ru>
2020-08-14 02:24:05 +0300

    network icons
    Signed-off-by: Ali Abdulmadzhidov <mrfreezzzer@mail.ru>

--- feature-account-impl/src/main/java/jp/co/soramitsu/feature_account_impl/presentation/view/advanced/network/model/NetworkModel.kt
+++ feature-account-impl/src/main/java/jp/co/soramitsu/feature_account_impl/presentation/view/advanced/network/model/NetworkModel.kt
@@ -5,6 +5,7 @@ import jp.co.soramitsu.feature_account_api.domain.model.NetworkType
 data class NetworkModel(
     val name: String,
     val icon: Int,
+    val smallIcon: Int,
     val link: String,
     val networkType: NetworkType,
     val isSelected: Boolean
