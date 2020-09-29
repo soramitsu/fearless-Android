083fe2f46026882c386fb8b4671f76ec27847bd6
Valentun <sergeev@soramitsu.co.jp>
2020-09-29 15:15:12 +0300

    Balance detail networking

--- core-db/src/main/java/jp/co/soramitsu/core_db/dao/AssetDao.kt
+++ core-db/src/main/java/jp/co/soramitsu/core_db/dao/AssetDao.kt
@@ -6,11 +6,15 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import io.reactivex.Observable
 import jp.co.soramitsu.core_db.model.AssetLocal
+import jp.co.soramitsu.feature_wallet_api.domain.model.Asset
 
 @Dao
 abstract class AssetDao {
     @Query("select * from assets where accountAddress = :accountAddress")
     abstract fun observeAssets(accountAddress: String): Observable<List<AssetLocal>>
+
+    @Query("select * from assets where accountAddress = :accountAddress and token = :token")
+    abstract fun observeAsset(accountAddress: String, token: Asset.Token): Observable<AssetLocal>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract fun insert(assets: List<AssetLocal>)
--- feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/model/AssetModel.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/model/AssetModel.kt
@@ -3,6 +3,7 @@ package jp.co.soramitsu.feature_wallet_impl.presentation.model
 import jp.co.soramitsu.common.utils.isNonNegative
 import jp.co.soramitsu.feature_wallet_api.domain.model.Asset
 import jp.co.soramitsu.feature_wallet_impl.R
+import jp.co.soramitsu.feature_wallet_impl.util.format
 import java.math.BigDecimal
 
 data class AssetModel(
@@ -10,23 +11,21 @@ data class AssetModel(
     val balance: BigDecimal,
     val dollarRate: BigDecimal?,
     val recentRateChange: BigDecimal?,
-    val dollarAmount: BigDecimal?
+    val dollarAmount: BigDecimal?,
+    val bonded: BigDecimal,
+    val available: BigDecimal
 ) {
     val rateChangeColorRes = determineChangeColor()
+
+    val formattedAmount = createFormattedAmount()
 
     private fun determineChangeColor(): Int? {
         if (recentRateChange == null) return null
 
         return if (recentRateChange.isNonNegative) R.color.green else R.color.red
     }
-}
 
-fun Asset.toUiModel(): AssetModel {
-    return AssetModel(
-        token = token,
-        balance = balance,
-        dollarRate = dollarRate,
-        recentRateChange = recentRateChange,
-        dollarAmount = dollarAmount
-    )
+    private fun createFormattedAmount(): String {
+        return "${balance.format()} ${token.displayName}"
+    }
 }
