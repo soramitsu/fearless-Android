adc237cd28c75028924ce36b4a8736d1d0c8ebbf
Valentun <sergeev@soramitsu.co.jp>
2020-10-22 11:25:41 +0300

    Localization

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/send/BalanceDetailsBottomSheet.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/send/BalanceDetailsBottomSheet.kt
@@ -16,7 +16,7 @@ class BalanceDetailsBottomSheet(
 
         setTitle(R.string.wallet_balance_details_title)
 
-        addCurrencyItem(R.string.wallet_balance_details_available, payload.available)
+        addCurrencyItem(R.string.choose_amount_available_balance, payload.available)
         addCurrencyItem(R.string.wallet_balance_details_total, payload.totalBalance)
         addCurrencyItem(R.string.wallet_balance_details_total_after, payload.totalBalanceAfterTransfer)
         addCurrencyItem(R.string.wallet_balance_details_existential_deposit, payload.token.networkType.existentialDeposit)
