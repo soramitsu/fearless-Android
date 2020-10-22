53c2532a366b7c58da6351ced62b7b652586da1f
Valentun <sergeev@soramitsu.co.jp>
2020-10-22 17:27:50 +0300

    Remove unused code

--- common/src/main/java/jp/co/soramitsu/common/account/AddressIconGenerator.kt
+++ common/src/main/java/jp/co/soramitsu/common/account/AddressIconGenerator.kt
@@ -1,6 +1,5 @@
 package jp.co.soramitsu.common.account
 
-import android.graphics.drawable.PictureDrawable
 import io.reactivex.Single
 import jp.co.soramitsu.common.resources.ResourceManager
 import jp.co.soramitsu.fearless_utils.icon.IconGenerator
@@ -14,13 +13,6 @@ class AddressIconGenerator(
             val sizeInPx = resourceManager.measureInPx(sizeInDp)
             val icon = iconGenerator.getSvgImage(addressId, sizeInPx)
             AddressModel(accountAddress, icon)
-        }
-    }
-
-    fun createAddressIcon(addressId: ByteArray, sizeInDp: Int): Single<PictureDrawable> {
-        return Single.fromCallable {
-            val sizeInPx = resourceManager.measureInPx(sizeInDp)
-            iconGenerator.getSvgImage(addressId, sizeInPx)
         }
     }
 }
