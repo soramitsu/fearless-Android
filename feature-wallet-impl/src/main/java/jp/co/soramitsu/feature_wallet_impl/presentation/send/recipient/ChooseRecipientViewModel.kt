5d01d5eae8b36f31deab3762acd6a39f71567d26
Valentun <sergeev@soramitsu.co.jp>
2020-10-26 15:52:28 +0300

    Reorder sections in search

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/send/recipient/ChooseRecipientViewModel.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/send/recipient/ChooseRecipientViewModel.kt
@@ -88,7 +88,7 @@ class ChooseRecipientViewModel(
                     val myAccountsWithHeader = generateModelsWithHeader(R.string.search_header_my_accounts, myAccounts)
                     val contactsWithHeader = generateModelsWithHeader(R.string.search_contacts, contacts)
 
-                    val result = myAccountsWithHeader + contactsWithHeader + resultWithHeader
+                    val result = resultWithHeader + myAccountsWithHeader + contactsWithHeader
 
                     result.zipSimilar()
                 }
