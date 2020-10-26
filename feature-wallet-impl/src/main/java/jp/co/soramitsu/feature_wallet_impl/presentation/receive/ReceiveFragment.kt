9115a2bde3d0895ebf89ee69652ae8b1c1f035ff
ArtemNeFRiT <Artem.NeFRiT@gmail.com>
2020-10-26 15:45:23 +0300

    added file provider, sync resources, formatted sharing message and creating share intent

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/receive/ReceiveFragment.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/receive/ReceiveFragment.kt
@@ -1,8 +1,12 @@
 package jp.co.soramitsu.feature_wallet_impl.presentation.receive
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.FileProvider
 import jp.co.soramitsu.common.base.BaseFragment
 import jp.co.soramitsu.common.di.FeatureUtils
 import jp.co.soramitsu.feature_wallet_api.di.WalletFeatureApi
@@ -11,8 +15,14 @@ import jp.co.soramitsu.feature_wallet_impl.di.WalletFeatureComponent
 import kotlinx.android.synthetic.main.fragment_receive.accountView
 import kotlinx.android.synthetic.main.fragment_receive.fearlessToolbar
 import kotlinx.android.synthetic.main.fragment_receive.qrImg
+import java.io.File
+import java.io.FileOutputStream
 
 class ReceiveFragment : BaseFragment<ReceiveViewModel>() {
+
+    companion object {
+        private const val IMAGE_NAME = "image.png"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -55,5 +65,32 @@ class ReceiveFragment : BaseFragment<ReceiveViewModel>() {
         viewModel.accountIconLiveData.observe {
             accountView.setAccountIcon(it.image)
         }
+
+        viewModel.shareEvent.observeEvent {
+            val mediaStorageDir: File? = saveToTempFile(activity!!, it.first)
+
+            if (mediaStorageDir != null) {
+                val imageUri = FileProvider.getUriForFile(activity!!, "${activity!!.packageName}.provider", mediaStorageDir)
+
+                if (imageUri != null) {
+                    val intent = Intent(Intent.ACTION_SEND).apply {
+                        type = "image/*"
+                        putExtra(Intent.EXTRA_STREAM, imageUri)
+                        putExtra(Intent.EXTRA_TEXT, it.second)
+                    }
+
+                    startActivity(Intent.createChooser(intent, getString(R.string.wallet_receive_description)))
+                }
+            }
+        }
+    }
+
+    private fun saveToTempFile(context: Context, bitmap: Bitmap): File? {
+        val mediaStorageDir = File(context.externalCacheDir!!.absolutePath + IMAGE_NAME)
+
+        val outputStream = FileOutputStream(mediaStorageDir)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+        outputStream.close()
+        return mediaStorageDir
     }
 }
--- feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/receive/ReceiveViewModel.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/receive/ReceiveViewModel.kt
@@ -1,6 +1,8 @@
 package jp.co.soramitsu.feature_wallet_impl.presentation.receive
 
+import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -9,7 +11,9 @@ import jp.co.soramitsu.common.account.AddressModel
 import jp.co.soramitsu.common.base.BaseViewModel
 import jp.co.soramitsu.common.resources.ClipboardManager
 import jp.co.soramitsu.common.resources.ResourceManager
+import jp.co.soramitsu.common.utils.Event
 import jp.co.soramitsu.common.utils.QrCodeGenerator
+import jp.co.soramitsu.common.utils.plusAssign
 import jp.co.soramitsu.feature_account_api.domain.model.Account
 import jp.co.soramitsu.feature_wallet_api.domain.interfaces.WalletInteractor
 import jp.co.soramitsu.feature_wallet_impl.R
@@ -37,6 +41,8 @@ class ReceiveViewModel(
     val accountIconLiveData: LiveData<AddressModel> = observeIcon(selectedAccountObservable)
         .asLiveData()
 
+    val shareEvent = MutableLiveData<Event<Pair<Bitmap, String>>>()
+
     fun addressCopyClicked() {
         accountLiveData.value?.let {
             clipboardManager.addToClipboard(it.address)
@@ -50,7 +56,17 @@ class ReceiveViewModel(
     }
 
     fun shareButtonClicked() {
-        // TODO: implement sharing here
+        qrBitmapLiveData.value?.let { qrBitmap ->
+            disposables += interactor.observeCurrentAsset()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    val message = generateMessage(it.token.displayName, it.token.networkType.readableName)
+                    shareEvent.value = Event(Pair(qrBitmap, message))
+                }, {
+
+                })
+        }
     }
 
     private fun getQrCodeSharingString() = interactor.getQrCodeSharingString()
@@ -71,5 +87,9 @@ class ReceiveViewModel(
                 }
             }
             .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    private fun generateMessage(network: String, token: String, address: String = ""): String {
+        return resourceManager.getString(R.string.wallet_receive_share_message).format(network, token) + address
     }
 }
