ce5917fbf7bfe95603e2fa15209ffb00edb8321f
valentunn <70131744+valentunn@users.noreply.github.com>
2021-02-10 14:50:37 +0300

    Fix not clickable recipient (#153)

---
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/send/phishing/warning/api/PhishingWarningMixin.kt
@@ -0,0 +1,20 @@
+package jp.co.soramitsu.feature_wallet_impl.presentation.send.phishing.warning.api
+
+import androidx.lifecycle.LiveData
+import jp.co.soramitsu.common.utils.Event
+
+interface PhishingWarningMixin {
+
+    val showPhishingWarning: LiveData<Event<String>>
+
+    suspend fun proceedOrShowPhishingWarning(address: String, onProceed: () -> Unit)
+}
+
+interface PhishingWarningPresentation : PhishingWarningMixin {
+
+    fun proceedAddress(address: String)
+}
+
+suspend fun PhishingWarningPresentation.proceedOrShowPhishingWarning(address: String) {
+    proceedOrShowPhishingWarning(address) { proceedAddress(address) }
+}
