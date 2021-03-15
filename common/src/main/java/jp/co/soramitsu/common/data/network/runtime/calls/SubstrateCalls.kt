37d3319ab133d997c2b05702b14786af4263bf24
Valentun <sergeev@soramitsu.co.jp>
2021-03-15 09:23:01 +0300

    Refactoring

--- common/src/main/java/jp/co/soramitsu/common/data/network/runtime/calls/SubstrateCalls.kt
+++ common/src/main/java/jp/co/soramitsu/common/data/network/runtime/calls/SubstrateCalls.kt
@@ -1,24 +1,19 @@
 package jp.co.soramitsu.common.data.network.runtime.calls
 
-import jp.co.soramitsu.common.data.network.runtime.model.ActiveEraInfo
 import jp.co.soramitsu.common.data.network.runtime.model.FeeResponse
-import jp.co.soramitsu.fearless_utils.runtime.Module
-import jp.co.soramitsu.fearless_utils.runtime.storageKey
 import jp.co.soramitsu.fearless_utils.wsrpc.SocketService
 import jp.co.soramitsu.fearless_utils.wsrpc.executeAsync
 import jp.co.soramitsu.fearless_utils.wsrpc.mappers.nonNull
 import jp.co.soramitsu.fearless_utils.wsrpc.mappers.pojo
-import jp.co.soramitsu.fearless_utils.wsrpc.mappers.scale
 import jp.co.soramitsu.fearless_utils.wsrpc.request.DeliveryType
 import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.author.SubmitExtrinsicRequest
 import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.chain.RuntimeVersion
 import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.chain.RuntimeVersionRequest
-import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.storage.GetStorageRequest
 import java.math.BigInteger
 
 @Suppress("EXPERIMENTAL_API_USAGE")
 class SubstrateCalls(
-    private val socketService: SocketService
+    private val socketService: SocketService,
 ) {
 
     suspend fun getExtrinsicFee(extrinsic: String): BigInteger {
@@ -46,15 +41,6 @@ class SubstrateCalls(
         val doubleResult = response.result as Double
 
         return doubleResult.toInt().toBigInteger()
-    }
-
-    suspend fun getActiveEra(): Long {
-        val key = Module.Staking.ActiveEra.storageKey()
-        val request = GetStorageRequest(listOf(key))
-
-        val struct = socketService.executeAsync(request, mapper = scale(ActiveEraInfo).nonNull())
-
-        return struct[ActiveEraInfo.index].toLong()
     }
 
     suspend fun getRuntimeVersion(): RuntimeVersion {
