aa15d17adf6444d6d6808c0432e190e894f63c3f
Valentun <sergeev@soramitsu.co.jp>
2021-03-19 07:25:42 +0300

    Validator summary state

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/presentation/staking/StakingFragment.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/presentation/staking/StakingFragment.kt
@@ -23,6 +23,7 @@ import kotlinx.android.synthetic.main.fragment_staking.stakingContainer
 import kotlinx.android.synthetic.main.fragment_staking.stakingEstimate
 import kotlinx.android.synthetic.main.fragment_staking.stakingNetworkInfo
 import kotlinx.android.synthetic.main.fragment_staking.stakingNominatorSummary
+import kotlinx.android.synthetic.main.fragment_staking.stakingValidatorSummary
 import kotlinx.android.synthetic.main.fragment_staking.startStakingBtn
 
 class StakingFragment : BaseFragment<StakingViewModel>() {
@@ -67,6 +68,7 @@ class StakingFragment : BaseFragment<StakingViewModel>() {
             startStakingBtn.setVisible(stakingState is WelcomeViewState)
             stakingEstimate.setVisible(stakingState is WelcomeViewState)
             stakingNominatorSummary.setVisible(stakingState is NominatorViewState)
+            stakingValidatorSummary.setVisible(stakingState is ValidatorViewState)
 
             when (stakingState) {
                 is NominatorViewState -> {
