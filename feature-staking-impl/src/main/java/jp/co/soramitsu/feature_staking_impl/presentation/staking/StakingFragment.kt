d05d29ac6aaa05507d81672fc03dc3049244f4d9
Valentun <sergeev@soramitsu.co.jp>
2021-03-22 11:41:46 +0300

    Fix estimate earnings UI and keyboard issues

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/presentation/staking/StakingFragment.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/presentation/staking/StakingFragment.kt
@@ -40,8 +40,6 @@ class StakingFragment : BaseFragment<StakingViewModel>() {
             type(statusBars = true) {
                 padding()
             }
-
-            consume(true)
         }
 
         stakingEstimate.hideAssetBalanceDollarAmount()
