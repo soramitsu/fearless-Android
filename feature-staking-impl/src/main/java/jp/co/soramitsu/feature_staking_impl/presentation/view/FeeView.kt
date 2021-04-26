d7670aa202b9efece53ef0daeef1574bd97daa12
Valentun <sergeev@soramitsu.co.jp>
2021-04-26 15:02:17 +0300

    Add TableCellView

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/presentation/view/FeeView.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/presentation/view/FeeView.kt
@@ -2,47 +2,34 @@ package jp.co.soramitsu.feature_staking_impl.presentation.view
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
-import androidx.constraintlayout.widget.ConstraintLayout
-import jp.co.soramitsu.common.utils.makeGone
-import jp.co.soramitsu.common.utils.makeVisible
-import jp.co.soramitsu.common.utils.setTextOrHide
 import jp.co.soramitsu.feature_staking_impl.R
 import jp.co.soramitsu.feature_staking_impl.presentation.common.fee.FeeStatus
-import kotlinx.android.synthetic.main.view_fee.view.feeContent
-import kotlinx.android.synthetic.main.view_fee.view.feeFiat
-import kotlinx.android.synthetic.main.view_fee.view.feeProgress
-import kotlinx.android.synthetic.main.view_fee.view.feeToken
 
 class FeeView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0,
-) : ConstraintLayout(context, attrs, defStyle) {
+) : TableCellView(context, attrs, defStyle) {
 
     init {
-        View.inflate(context, R.layout.view_fee, this)
+        setTitle(R.string.network_fee)
+
+        setFeeStatus(FeeStatus.Loading)
     }
 
     fun setFeeStatus(feeStatus: FeeStatus) {
         when (feeStatus) {
             is FeeStatus.Loading -> {
-                feeContent.makeGone()
-                feeProgress.makeVisible()
+                showProgress()
             }
             is FeeStatus.Error -> {
-                feeToken.text = context.getString(R.string.common_error_general_title)
-
-                feeToken.makeVisible()
-                feeFiat.makeGone()
-                feeProgress.makeGone()
+                showValue(context.getString(R.string.common_error_general_title))
             }
             is FeeStatus.Loaded -> {
-                feeContent.makeVisible()
-                feeToken.text = feeStatus.feeModel.displayToken
-                feeFiat.setTextOrHide(feeStatus.feeModel.displayFiat)
-
-                feeProgress.makeGone()
+                showValue(
+                    primary = feeStatus.feeModel.displayToken,
+                    secondary = feeStatus.feeModel.displayFiat
+                )
             }
         }
     }
