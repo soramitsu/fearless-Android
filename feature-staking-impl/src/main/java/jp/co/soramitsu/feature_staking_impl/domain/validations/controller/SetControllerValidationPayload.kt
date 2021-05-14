618e6adbeaa9e8cb2dfa7337b5b6190583d4c74d
Ekaterina Levchenko <e.levchenko@innopolis.ru>
2021-05-14 14:21:13 +0300

    Button fix

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/domain/validations/controller/SetControllerValidationPayload.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/domain/validations/controller/SetControllerValidationPayload.kt
@@ -1,6 +1,5 @@
 package jp.co.soramitsu.feature_staking_impl.domain.validations.controller
 
-import jp.co.soramitsu.feature_staking_api.domain.model.StakingState
 import java.math.BigDecimal
 
 class SetControllerValidationPayload(
