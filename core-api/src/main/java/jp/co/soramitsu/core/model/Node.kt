da3a559e88db450ebe59483ea0fc36406bb1fcfe
Ekaterina Levchenko <e.levchenko@innopolis.ru>
2021-05-21 18:47:39 +0300

    DAO way

--- core-api/src/main/java/jp/co/soramitsu/core/model/Node.kt
+++ core-api/src/main/java/jp/co/soramitsu/core/model/Node.kt
@@ -50,3 +50,10 @@ data class Node(
         }
     }
 }
+
+fun Node.NetworkType.getSubqueryPath() =
+    when(this){
+        Node.NetworkType.POLKADOT -> "sum-reward"
+        Node.NetworkType.KUSAMA -> "sum-reward-kusama"
+        else -> null
+    }
