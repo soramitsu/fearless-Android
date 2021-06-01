247e5b40a1462147a16100c27e11f4b8410a021a
Valentun <sergeev@soramitsu.co.jp>
2021-06-01 14:20:04 +0300

    Fix -  Switch from current network to default if change account.

--- feature-account-impl/src/main/java/jp/co/soramitsu/feature_account_impl/domain/AccountInteractorImpl.kt
+++ feature-account-impl/src/main/java/jp/co/soramitsu/feature_account_impl/domain/AccountInteractorImpl.kt
@@ -148,7 +148,7 @@ class AccountInteractorImpl(
     override suspend fun selectAccount(address: String) {
         val account = accountRepository.getAccount(address)
 
-        selectAccount(account)
+        accountRepository.selectAccount(account)
     }
 
     override suspend fun updateAccountName(account: Account, newName: String) {
@@ -179,13 +179,6 @@ class AccountInteractorImpl(
         if (account.address == newAccount.address) {
             accountRepository.selectAccount(newAccount)
         }
-    }
-
-    private suspend fun selectAccount(account: Account) {
-        val node = accountRepository.getDefaultNode(account.network.type)
-
-        accountRepository.selectNode(node)
-        accountRepository.selectAccount(account)
     }
 
     private suspend fun mergeAccountsWithNetworks(accounts: List<Account>): List<Any> {
@@ -273,8 +266,7 @@ class AccountInteractorImpl(
         val account = accountRepository.getAccount(accountAddress)
         val node = accountRepository.getNode(nodeId)
 
-        accountRepository.selectNode(node)
-        accountRepository.selectAccount(account)
+        accountRepository.selectAccount(account, newNode = node)
     }
 
     override suspend fun selectNode(nodeId: Int) {
