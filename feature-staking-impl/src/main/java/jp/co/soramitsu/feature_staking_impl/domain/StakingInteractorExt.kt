3ff012e023f1c529729e2d9313af253cff3a2160
Valentun <sergeev@soramitsu.co.jp>
2021-06-02 14:02:51 +0300

    Refactoring

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/domain/StakingInteractorExt.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/domain/StakingInteractorExt.kt
@@ -1,10 +1,11 @@
 package jp.co.soramitsu.feature_staking_impl.domain
 
+import jp.co.soramitsu.fearless_utils.runtime.AccountId
 import jp.co.soramitsu.feature_staking_api.domain.model.Exposure
 import jp.co.soramitsu.feature_staking_api.domain.model.IndividualExposure
 
 fun isNominationActive(
-    stashId: ByteArray,
+    stashId: AccountId,
     exposures: Collection<Exposure>,
     rewardedNominatorsPerValidator: Int
 ): Boolean {
@@ -16,9 +17,22 @@ fun isNominationActive(
         exposure.others.any(comparator)
     }
 
-    return validatorsWithOurStake.any {
-        val numberInRewardedList = it.others.sortedByDescending(IndividualExposure::value).indexOfFirst(comparator) + 1
+    return validatorsWithOurStake.any { it.willAccountBeRewarded(stashId, rewardedNominatorsPerValidator) }
+}
 
-        numberInRewardedList <= rewardedNominatorsPerValidator
+fun Exposure.willAccountBeRewarded(
+    accountId: AccountId,
+    rewardedNominatorsPerValidator: Int
+): Boolean {
+    val indexInRewardedList = others.sortedByDescending(IndividualExposure::value).indexOfFirst {
+        it.who.contentEquals(accountId)
     }
+
+    if (indexInRewardedList == -1) {
+        return false
+    }
+
+    val numberInRewardedList = indexInRewardedList + 1
+
+    return numberInRewardedList <= rewardedNominatorsPerValidator
 }
