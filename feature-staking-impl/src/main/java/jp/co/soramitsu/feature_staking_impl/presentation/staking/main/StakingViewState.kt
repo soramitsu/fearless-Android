870cab8d051f61c44616af83582d6eabb0b62b40
FrogTravel <e.levchenko@innopolis.ru>
2021-06-24 11:36:09 +0300

    Bug fix. Now total rewards are requested for stash account, not for current account (#323)

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/presentation/staking/main/StakingViewState.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/presentation/staking/main/StakingViewState.kt
@@ -127,7 +127,7 @@ sealed class StakeViewState<S>(
 
     private fun syncStakingRewards() {
         scope.launch {
-            val syncResult = stakingInteractor.syncStakingRewards(stakeState.accountAddress)
+            val syncResult = stakingInteractor.syncStakingRewards(stakeState.stashAddress)
 
             syncResult.exceptionOrNull()?.let { errorDisplayer(it) }
         }
