7fe5d423621069e46639f10d56894843c9b59754
Ekaterina Levchenko <e.levchenko@innopolis.ru>
2021-07-27 20:48:59 +0300

    Updater returned; Attempt to fix bug with new era crash

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/di/StakingUpdatersModule.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/di/StakingUpdatersModule.kt
@@ -29,6 +29,7 @@ import jp.co.soramitsu.feature_staking_impl.data.network.blockhain.updaters.Stak
 import jp.co.soramitsu.feature_staking_impl.data.network.blockhain.updaters.TotalIssuanceUpdater
 import jp.co.soramitsu.feature_staking_impl.data.network.blockhain.updaters.ValidatorExposureUpdater
 import jp.co.soramitsu.feature_staking_impl.data.network.blockhain.updaters.controller.AccountControllerBalanceUpdater
+import jp.co.soramitsu.feature_staking_impl.data.network.blockhain.updaters.historical.HistoricalTotalValidatorRewardUpdater
 import jp.co.soramitsu.feature_staking_impl.data.network.blockhain.updaters.historical.HistoricalUpdateMediator
 import jp.co.soramitsu.feature_staking_impl.data.network.blockhain.updaters.historical.HistoricalValidatorRewardPointsUpdater
 import jp.co.soramitsu.feature_staking_impl.data.network.blockhain.updaters.scope.AccountStakingScope
@@ -159,6 +160,7 @@ class StakingUpdatersModule {
         storageCache: StorageCache,
     ) = HistoricalUpdateMediator(
         historicalUpdaters = listOf(
+            HistoricalTotalValidatorRewardUpdater(),
             HistoricalValidatorRewardPointsUpdater(),
         ),
         runtimeProperty = runtimeProperty,
