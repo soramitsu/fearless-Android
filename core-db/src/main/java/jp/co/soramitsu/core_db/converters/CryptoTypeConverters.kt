e8af61a5439cbc6a34ba46d99b4f46f92238caa1
Valentun <sergeev@soramitsu.co.jp>
2021-08-31 14:27:45 +0300

    Code style

--- core-db/src/main/java/jp/co/soramitsu/core_db/converters/CryptoTypeConverters.kt
+++ core-db/src/main/java/jp/co/soramitsu/core_db/converters/CryptoTypeConverters.kt
@@ -5,9 +5,9 @@ import jp.co.soramitsu.core.model.CryptoType
 
 class CryptoTypeConverters {
 
-        @TypeConverter
-        fun from(cryptoType: CryptoType): String = cryptoType.name
+    @TypeConverter
+    fun from(cryptoType: CryptoType): String = cryptoType.name
 
-        @TypeConverter
-        fun to(name: String): CryptoType = enumValueOf(name)
+    @TypeConverter
+    fun to(name: String): CryptoType = enumValueOf(name)
 }
