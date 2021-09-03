93981579cb23c0a95f48dfea6e3c98bdd59f3dd2
Valentun <sergeev@soramitsu.co.jp>
2021-09-03 19:11:52 +0300

    Update westend subquery project path

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/data/repository/NodeSubqueryExtensions.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/data/repository/NodeSubqueryExtensions.kt
@@ -8,6 +8,6 @@ fun Node.NetworkType.subqueryFearlessApiPath() =
     when (this) {
         Node.NetworkType.POLKADOT -> "fearless-wallet"
         Node.NetworkType.KUSAMA -> "fearless-wallet-ksm"
-        Node.NetworkType.WESTEND -> "fearless-wallet-westend__ZWYxc"
+        Node.NetworkType.WESTEND -> "fearless-wallet-westend"
         else -> throw WrongNetworkTypeForSubqueryRequest("$this is not supported for fetching pending rewards via Subquery")
     }
