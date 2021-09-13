68ec86f2f442c8fc76c12572c15faae7247f30ab
Valentun <sergeev@soramitsu.co.jp>
2021-09-13 10:54:38 +0300

    Code style

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/data/network/model/request/SubqueryExpressions.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/data/network/model/request/SubqueryExpressions.kt
@@ -3,7 +3,7 @@ package jp.co.soramitsu.feature_wallet_impl.data.network.model.request
 object SubqueryExpressions {
 
     fun or(vararg innerExpressions: String): String {
-       return compoundExpression("or", *innerExpressions)
+        return compoundExpression("or", *innerExpressions)
     }
 
     fun or(innerExpressions: Collection<String>) = or(*innerExpressions.toTypedArray())
@@ -11,13 +11,13 @@ object SubqueryExpressions {
     fun anyOf(innerExpressions: Collection<String>) = or(innerExpressions)
 
     fun and(vararg innerExpressions: String): String {
-       return compoundExpression("and", *innerExpressions)
+        return compoundExpression("and", *innerExpressions)
     }
 
     fun and(innerExpressions: Collection<String>) = and(*innerExpressions.toTypedArray())
 
     fun not(expression: String): String {
-        return "not: {${expression}}"
+        return "not: {$expression}"
     }
 
     private fun compoundExpression(name: String, vararg innerExpressions: String): String {
