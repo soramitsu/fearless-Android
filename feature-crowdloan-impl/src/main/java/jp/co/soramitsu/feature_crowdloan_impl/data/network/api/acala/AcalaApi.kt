cd99cdab943208954937819cb3e9a0d05eb7581a
pankratov <sergbk17@mail.ru>
2021-10-21 11:34:45 +0500

    acala api

---
+++ feature-crowdloan-impl/src/main/java/jp/co/soramitsu/feature_crowdloan_impl/data/network/api/acala/AcalaApi.kt
@@ -0,0 +1,33 @@
+package jp.co.soramitsu.feature_crowdloan_impl.data.network.api.acala
+
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface AcalaApi {
+
+    companion object {
+        const val BASE_URL = "https://crowdloan.aca-dev.network"
+    }
+
+    @GET("/statement")
+    suspend fun getStatement(): AcalaStatement
+
+    @GET("/referral/{referral}")
+    suspend fun isReferralValid(
+        @Path("referral") referral: String
+    ): AcalaReferralCheck
+
+    //submit the user’s referral code and the signature of the terms
+    @POST("/contribute")
+    suspend fun contribute(
+        @Body body: AcalaContributeRequest
+    ): AcalaContributeResponse
+
+    //record the emails of people who submit contributions via the Liquid Crowdloan DOT
+    @POST("/transfer")
+    suspend fun transfer(
+        @Body body: AcalaTransferRequest
+    ): AcalaContributeResponse
+}
