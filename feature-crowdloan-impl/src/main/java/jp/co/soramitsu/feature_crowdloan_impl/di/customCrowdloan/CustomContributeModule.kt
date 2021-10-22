e2ecfde2d5f49d5abfce719ce861aaaaca89e4e6
pankratov <pankratov@soramitsu.co.jp>
2021-10-22 19:07:20 +0500

    acala and moonbeam crowdlones custom factory module

--- feature-crowdloan-impl/src/main/java/jp/co/soramitsu/feature_crowdloan_impl/di/customCrowdloan/CustomContributeModule.kt
+++ feature-crowdloan-impl/src/main/java/jp/co/soramitsu/feature_crowdloan_impl/di/customCrowdloan/CustomContributeModule.kt
@@ -3,13 +3,17 @@ package jp.co.soramitsu.feature_crowdloan_impl.di.customCrowdloan
 import dagger.Module
 import dagger.Provides
 import jp.co.soramitsu.common.di.scope.FeatureScope
+import jp.co.soramitsu.feature_crowdloan_impl.di.customCrowdloan.acala.AcalaContributionModule
 import jp.co.soramitsu.feature_crowdloan_impl.di.customCrowdloan.bifrost.BifrostContributionModule
 import jp.co.soramitsu.feature_crowdloan_impl.di.customCrowdloan.karura.KaruraContributionModule
+import jp.co.soramitsu.feature_crowdloan_impl.di.customCrowdloan.moonbeam.MoonbeamContributionModule
 
 @Module(
     includes = [
         KaruraContributionModule::class,
-        BifrostContributionModule::class
+        BifrostContributionModule::class,
+        AcalaContributionModule::class,
+        MoonbeamContributionModule::class
     ]
 )
 class CustomContributeModule {
