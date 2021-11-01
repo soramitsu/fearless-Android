d2785995589d1fa6cd7f934ed95d262f5d68cc60
Arvi Fox <arvifox@arvifox.com>
2021-11-01 19:43:01 +0300

    crowdloan

--- feature-crowdloan-impl/src/main/java/jp/co/soramitsu/feature_crowdloan_impl/data/network/blockhain/extrinsic/ExtrinsicBuilderExt.kt
+++ feature-crowdloan-impl/src/main/java/jp/co/soramitsu/feature_crowdloan_impl/data/network/blockhain/extrinsic/ExtrinsicBuilderExt.kt
@@ -7,17 +7,24 @@ import jp.co.soramitsu.fearless_utils.runtime.extrinsic.ExtrinsicBuilder
 import jp.co.soramitsu.feature_crowdloan_api.data.network.blockhain.binding.ParaId
 import java.math.BigInteger
 
-fun ExtrinsicBuilder.contribute(parachainId: ParaId, contribution: BigInteger, signature: String? = null, encryptionType: EncryptionType? = null): ExtrinsicBuilder {
+fun ExtrinsicBuilder.contribute(
+    parachainId: ParaId,
+    contribution: BigInteger,
+    signature: String? = null,
+    encryptionType: EncryptionType? = null
+): ExtrinsicBuilder {
     return call(
         moduleName = "Crowdloan",
         callName = "contribute",
         arguments = mapOf(
             "index" to parachainId,
             "value" to contribution,
-            "signature" to DictEnum.Entry(
-                name = encryptionType?.rawName?.capitalize().orEmpty(),
-                value = signature?.fromHex()
-            )
+            "signature" to signature?.let {
+                DictEnum.Entry(
+                    name = encryptionType?.rawName?.capitalize().orEmpty(),
+                    value = it.fromHex()
+                )
+            }
         )
     )
 }
