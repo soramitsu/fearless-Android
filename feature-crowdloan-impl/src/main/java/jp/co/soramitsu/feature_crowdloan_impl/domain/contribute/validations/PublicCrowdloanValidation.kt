8ea6f295600a5b05975936b6f00bf7318b69da31
pankratov <pankratov@soramitsu.co.jp>
2021-11-04 02:21:13 +0500

    allow private crowdloan for moonbeam on prod (validation rule change)

--- feature-crowdloan-impl/src/main/java/jp/co/soramitsu/feature_crowdloan_impl/domain/contribute/validations/PublicCrowdloanValidation.kt
+++ feature-crowdloan-impl/src/main/java/jp/co/soramitsu/feature_crowdloan_impl/domain/contribute/validations/PublicCrowdloanValidation.kt
@@ -2,14 +2,13 @@ package jp.co.soramitsu.feature_crowdloan_impl.domain.contribute.validations
 
 import jp.co.soramitsu.common.validation.ValidationStatus
 import jp.co.soramitsu.common.validation.validOrError
-import jp.co.soramitsu.feature_crowdloan_impl.BuildConfig
 
 class PublicCrowdloanValidation : ContributeValidation {
 
     override suspend fun validate(value: ContributeValidationPayload): ValidationStatus<ContributeValidationFailure> {
-        val isDebugMoonbeam = BuildConfig.DEBUG && value.crowdloan.parachainMetadata?.isMoonbeam == true
+        val isMoonbeam = value.crowdloan.parachainMetadata?.isMoonbeam == true
         val hasNoVerifier = value.crowdloan.fundInfo.verifier == null
-        return validOrError(isDebugMoonbeam || hasNoVerifier) {
+        return validOrError(isMoonbeam || hasNoVerifier) {
             ContributeValidationFailure.PrivateCrowdloanNotSupported
         }
     }
