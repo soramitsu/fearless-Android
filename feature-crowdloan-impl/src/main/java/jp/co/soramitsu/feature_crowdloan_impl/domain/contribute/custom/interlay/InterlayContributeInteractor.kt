0bc785707f445122b3126f0f6025bf33c5b6f3e6
Sergey Pankratov <pankratov@soramitsu.co.jp>
2021-12-13 22:06:38 +0500

    change addMemo

--- feature-crowdloan-impl/src/main/java/jp/co/soramitsu/feature_crowdloan_impl/domain/contribute/custom/interlay/InterlayContributeInteractor.kt
+++ feature-crowdloan-impl/src/main/java/jp/co/soramitsu/feature_crowdloan_impl/domain/contribute/custom/interlay/InterlayContributeInteractor.kt
@@ -1,5 +1,6 @@
 package jp.co.soramitsu.feature_crowdloan_impl.domain.contribute.custom.interlay
 
+import jp.co.soramitsu.fearless_utils.extensions.fromHex
 import jp.co.soramitsu.fearless_utils.runtime.extrinsic.ExtrinsicBuilder
 import jp.co.soramitsu.feature_crowdloan_api.data.network.blockhain.binding.ParaId
 import jp.co.soramitsu.feature_crowdloan_impl.data.network.blockhain.extrinsic.addMemo
@@ -15,6 +16,6 @@ class InterlayContributeInteractor {
         referralCode: String,
         extrinsicBuilder: ExtrinsicBuilder
     ) = withContext(Dispatchers.Default) {
-        extrinsicBuilder.addMemo(paraId, referralCode.toByteArray())
+        extrinsicBuilder.addMemo(paraId, referralCode.fromHex())
     }
 }
