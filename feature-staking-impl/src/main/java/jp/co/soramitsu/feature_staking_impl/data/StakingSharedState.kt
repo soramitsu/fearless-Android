4c46c9b5b7a27e125dcc1347ae70013ad32c7cb5
Sergey Pankratov <pankratov@soramitsu.co.jp>
2021-12-23 16:04:55 +0500

    revert fix staking

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/data/StakingSharedState.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/data/StakingSharedState.kt
@@ -6,7 +6,6 @@ import jp.co.soramitsu.runtime.multiNetwork.chain.model.Chain
 import jp.co.soramitsu.runtime.state.SingleAssetSharedState
 
 private const val STAKING_SHARED_STATE = "STAKING_SHARED_STATE"
-private const val localTestPolkadotChainId = "111979a679f11fc283d8968b657855f4afab1cff6905c7cb0451bdf2c50df47c"//dot
 
 class StakingSharedState(
     chainRegistry: ChainRegistry,
@@ -14,6 +13,6 @@ class StakingSharedState(
 ) : SingleAssetSharedState(
     preferences = preferences,
     chainRegistry = chainRegistry,
-    filter = { _, chainAsset -> chainAsset.staking != Chain.Asset.StakingType.UNSUPPORTED && chainAsset.chainId != localTestPolkadotChainId},//todo remove local test net from  staking
+    filter = { _, chainAsset -> chainAsset.staking != Chain.Asset.StakingType.UNSUPPORTED },
     preferencesKey = STAKING_SHARED_STATE
 )
