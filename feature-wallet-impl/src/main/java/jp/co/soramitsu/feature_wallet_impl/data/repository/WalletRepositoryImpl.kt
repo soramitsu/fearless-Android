fee70739a9cb3e4fd79d379fe463307e1823c3e6
Sergey Pankratov <pankratov@soramitsu.co.jp>
2021-12-29 18:52:19 +0300

    change assets sort: native is first

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/data/repository/WalletRepositoryImpl.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/data/repository/WalletRepositoryImpl.kt
@@ -85,7 +85,9 @@ class WalletRepositoryImpl(
                 it.token.configuration.chainToSymbol !in updatedAssets.map { it.token.configuration.chainToSymbol }
             }
 
-            (updatedAssets + notUpdatedAssets).sortedWith(compareBy({ it.token.configuration.symbol }, { it.token.configuration.chainId }))
+            (updatedAssets + notUpdatedAssets).sortedWith(compareBy(
+                { it.token.configuration.symbol }, { !it.token.configuration.isNative }, { it.token.configuration.chainId })
+            )
         }
     }
 
