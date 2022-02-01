80853131198cb32c70391149d9887f9d72fe61e3
Denis Lyazgin <lyazgin@soramitsu.co.jp>
2022-02-01 23:48:45 +0700

    fix app errors, make it launch

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/beacon/main/PermissionRequestBottomSheet.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/beacon/main/PermissionRequestBottomSheet.kt
@@ -2,13 +2,10 @@ package jp.co.soramitsu.feature_wallet_impl.presentation.beacon.main
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
-import jp.co.soramitsu.common.utils.format
 import jp.co.soramitsu.common.view.bottomSheet.list.fixed.FixedListBottomSheet
-import jp.co.soramitsu.common.view.bottomSheet.list.fixed.item
 import jp.co.soramitsu.feature_wallet_impl.R
-import kotlinx.android.synthetic.main.item_sheet_currency.view.itemCurrencyLabel
-import kotlinx.android.synthetic.main.item_sheet_currency.view.itemCurrencyValue
 
 class PermissionRequestBottomSheet(
     context: Context,
@@ -25,14 +22,14 @@ class PermissionRequestBottomSheet(
 //        item(titleRes = R.string.common_connect) {
 //            onApprove()
 //        }
-        item(TextView(context)) { view ->//todo fix
-            view.itemCurrencyLabel.text = "common_connect"
-            view.setOnClickListener { onApprove() }
+        item(R.layout.item_text_header) { view ->//todo fix
+            view.textHeader.text = "common_connect"
+            view.setDismissingClickListener { onApprove() }
         }
 
-        item(TextView(context)) { view ->//todo fix
-            view.itemCurrencyLabel.setText(R.string.common_cancel)
-            view.setOnClickListener { onDeny() }
+        item(R.layout.item_text_header) { view ->//todo fix
+            view.textHeader.setText(R.string.common_cancel)
+            view.setDismissingClickListener { onDeny() }
         }
 
 //        item(titleRes = R.string.common_cancel) {
@@ -41,4 +38,7 @@ class PermissionRequestBottomSheet(
 
         setCancelable(false)
     }
+
+    private val View.textHeader
+        get() = findViewById<TextView>(R.id.textHeader)
 }
