c5cd954b1de23b435835c5a041762cc2ec85eeb4
Sergey Pankratov <pankratov@soramitsu.co.jp>
2022-05-24 11:56:53 +0300

    candidate info

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/data/network/blockhain/bindings/StakingMinMaxStorages.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/data/network/blockhain/bindings/StakingMinMaxStorages.kt
@@ -2,8 +2,13 @@ package jp.co.soramitsu.feature_staking_impl.data.network.blockhain.bindings
 
 import jp.co.soramitsu.common.data.network.runtime.binding.bindNumber
 import jp.co.soramitsu.common.data.network.runtime.binding.fromHexOrIncompatible
+import jp.co.soramitsu.common.data.network.runtime.binding.getTyped
+import jp.co.soramitsu.common.data.network.runtime.binding.incompatible
+import jp.co.soramitsu.common.data.network.runtime.binding.storageReturnType
 import jp.co.soramitsu.fearless_utils.runtime.RuntimeSnapshot
 import jp.co.soramitsu.fearless_utils.runtime.definitions.types.Type
+import jp.co.soramitsu.fearless_utils.runtime.definitions.types.composite.Struct
+import jp.co.soramitsu.fearless_utils.runtime.definitions.types.fromHexOrNull
 import java.math.BigInteger
 
 fun bindMinBond(scale: String, runtimeSnapshot: RuntimeSnapshot, type: Type<*>): BigInteger {
@@ -16,6 +21,27 @@ fun bindMaxNominators(scale: String, runtimeSnapshot: RuntimeSnapshot, type: Typ
 
 fun bindNominatorsCount(scale: String, runtimeSnapshot: RuntimeSnapshot, type: Type<*>): BigInteger {
     return bindNumber(scale, runtimeSnapshot, type)
+}
+
+fun bindRound(
+    scale: String,
+    runtime: RuntimeSnapshot
+): BigInteger {
+    val returnType = runtime.metadata.storageReturnType("ParachainStaking", "Round")
+    val decoded = returnType.fromHexOrNull(runtime, scale) as? Struct.Instance ?: incompatible()
+
+    return bindNumber(decoded.getTyped("current"))
+}
+
+fun bindSelectedCandidates(
+    scale: String,
+    runtime: RuntimeSnapshot
+): List<ByteArray> {
+    val returnType = runtime.metadata.storageReturnType("ParachainStaking", "SelectedCandidates")
+    @Suppress("UNCHECKED_CAST")
+    val decoded = returnType.fromHexOrNull(runtime, scale) as? List<ByteArray> ?: incompatible()
+
+    return decoded
 }
 
 private fun bindNumber(scale: String, runtimeSnapshot: RuntimeSnapshot, type: Type<*>): BigInteger {
