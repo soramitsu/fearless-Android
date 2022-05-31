6cf5709ccec69bb99c1a13370420839b7cc08ba3
Denis Lyazgin <lyazgin@soramitsu.co.jp>
2022-05-31 23:50:41 +0700

    fix rebase conflicts

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/domain/validators/CollatorProvider.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/domain/validators/CollatorProvider.kt
@@ -9,11 +9,12 @@ import jp.co.soramitsu.feature_staking_api.domain.model.Collator
 import jp.co.soramitsu.feature_staking_impl.data.repository.StakingConstantsRepository
 import jp.co.soramitsu.feature_staking_impl.domain.rewards.RewardCalculatorFactory
 import jp.co.soramitsu.feature_staking_impl.presentation.mappers.toCollator
+import jp.co.soramitsu.feature_staking_impl.scenarios.StakingParachainScenarioRepository
 import jp.co.soramitsu.runtime.multiNetwork.chain.model.Chain
 import kotlinx.coroutines.flow.first
 
 class CollatorProvider(
-    private val stakingRepository: StakingRepository,
+    private val stakingParachainScenarioRepository: StakingParachainScenarioRepository,
     private val identityRepository: IdentityRepository,
     private val rewardCalculatorFactory: RewardCalculatorFactory,
     private val stakingConstantsRepository: StakingConstantsRepository,
@@ -28,8 +29,8 @@ class CollatorProvider(
 
         val maxTopDelegationsPerCandidate = stakingConstantsRepository.maxTopDelegationsPerCandidate(chainId)
         val maxBottomDelegationsPerCandidate = stakingConstantsRepository.maxBottomDelegationsPerCandidate(chainId)
-        val selectedCandidates = stakingRepository.observeSelectedCandidates(chainId).first().plus("0xDA42293efa4a1bEd74B37317979BA14CE1D242b1".fromHex())
-        val candidateInfos = stakingRepository.getCandidateInfos(chainId, selectedCandidates)
+        val selectedCandidates = stakingParachainScenarioRepository.observeSelectedCandidates(chainId).first().plus("0xDA42293efa4a1bEd74B37317979BA14CE1D242b1".fromHex())
+        val candidateInfos = stakingParachainScenarioRepository.getCandidateInfos(chainId, selectedCandidates)
         val collators = candidateInfos.mapValuesNotNull {
             it.value?.toCollator(it.key)
         }
