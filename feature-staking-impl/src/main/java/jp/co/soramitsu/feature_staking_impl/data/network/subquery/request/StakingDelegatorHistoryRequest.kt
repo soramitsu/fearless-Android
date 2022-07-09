93dcee6046e3d8c5784a1e6dea75f909fe2a2a47
Sergey Pankratov <pankratov@soramitsu.co.jp>
2022-07-09 10:43:21 +0500

    FLW-1845 fixes in logic Staking. Basic flow for the staking management. History

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/data/network/subquery/request/StakingDelegatorHistoryRequest.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/data/network/subquery/request/StakingDelegatorHistoryRequest.kt
@@ -1,12 +1,14 @@
 package jp.co.soramitsu.feature_staking_impl.data.network.subquery.request
 
+import jp.co.soramitsu.fearless_utils.extensions.requireHexPrefix
+
 class StakingDelegatorHistoryRequest(delegatorAddress: String) {
     val query = """
     query {
         delegatorHistoryElements(
         last: 20,
         filter: {
-            delegatorId: { equalToInsensitive: "$delegatorAddress"}  }
+            delegatorId: { equalToInsensitive: "${delegatorAddress.requireHexPrefix()}"}  }
         ) {
             nodes {
               id
