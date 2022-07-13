13cf420f734130c15d4e67ee962c08038fb205e4
Sergey Pankratov <pankratov@soramitsu.co.jp>
2022-07-13 18:41:48 +0500

    FLW-1965 Staking: Connect SubQuery projects - add APR to collator lists

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/domain/validators/CollatorProvider.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/domain/validators/CollatorProvider.kt
@@ -1,7 +1,6 @@
 package jp.co.soramitsu.feature_staking_impl.domain.validators
 
 import jp.co.soramitsu.common.utils.mapValuesNotNull
-import jp.co.soramitsu.fearless_utils.extensions.toHexString
 import jp.co.soramitsu.feature_staking_api.domain.api.AccountIdMap
 import jp.co.soramitsu.feature_staking_api.domain.api.IdentityRepository
 import jp.co.soramitsu.feature_staking_api.domain.model.Collator
@@ -29,13 +28,10 @@ class CollatorProvider(
         val selectedCandidates = stakingParachainScenarioRepository.getSelectedCandidates(chainId) ?: return emptyMap()
         val candidateInfos = stakingParachainScenarioRepository.getCandidateInfos(chainId, selectedCandidates)
         val identityInfo = identityRepository.getIdentitiesFromIdsBytes(chain, selectedCandidates)
-        val apy = selectedCandidates.map {
-            val accountId = it.toHexString(true)
-            accountId to rewardCalculator.getApyFor(accountId)
-        }.toMap()
+        val apyMap = rewardCalculator.getApy(selectedCandidates)
 
         val collators = candidateInfos.mapValuesNotNull {
-            it.value.toCollator(it.key, identityInfo[it.key], apy[it.key])
+            it.value.toCollator(it.key, identityInfo[it.key], apyMap[it.key])
         }
         return collators
     }
