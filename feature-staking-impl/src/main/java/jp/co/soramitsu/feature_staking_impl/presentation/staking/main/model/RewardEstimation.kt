a966a6c402abf92b2aa091a9c8795f612a551f6a
Sergey Pankratov <pankratov@soramitsu.co.jp>
2022-07-27 17:42:52 +0500

    fix FLW-2141 Staking. After changing the wallet, the data flashes on the Staking screen

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/presentation/staking/main/model/RewardEstimation.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/presentation/staking/main/model/RewardEstimation.kt
@@ -1,6 +1,6 @@
 package jp.co.soramitsu.feature_staking_impl.presentation.staking.main.model
 
-class RewardEstimation(
+data class RewardEstimation(
     val amount: String,
     val fiatAmount: String?,
     val gain: String,
