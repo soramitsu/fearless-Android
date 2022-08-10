5e54a60d4fa8610d586c04679cea88cbf850902d
Sergey Pankratov <pankratov@soramitsu.co.jp>
2022-08-10 23:39:52 +0500

    add themes

---
+++ common/src/main/java/jp/co/soramitsu/common/compose/theme/Color.kt
@@ -0,0 +1,42 @@
+package jp.co.soramitsu.common.compose.theme
+
+import androidx.compose.ui.graphics.Color
+
+val colorPrimary = Color(0xFF004CB7)
+val colorSelected = Color(0x66FF009A)
+val colorPrimaryDark = Color(0x80004CB7)
+val colorAccent = Color(0xFFFF009A)
+val colorHint = Color(0xFFCCCCCC)
+val gray1 = Color(0xFFCCCCCC)
+val gray2 = Color(0xFF888888)
+val gray3 = Color(0xFF444444)
+val gray4 = Color(0xFF181818)
+val grayDisabled = Color(0xFFD0D0D0)
+val colorGreyText = Color(0xFF888888)
+val black = Color(0xFF000000)
+val green = Color(0xFF0ED33B)
+val dividerGray = Color(0xFF444444)
+val bottomSheetBackground = Color(0x80838383)
+val red = Color(0xFFF00004)
+val blurColorDark = Color(0xB3000000)
+val blurColor = Color(0x8C000000)
+val blurColorLight = Color(0x66000000)
+
+val white = Color(0xFFFFFFFF)
+val white10 = Color(0x1AFFFFFF)
+val white16 = Color(0x29FFFFFF)
+val white20 = Color(0x33FFFFFF)
+val white24 = Color(0x3DFFFFFF)
+val white30 = Color(0x4DFFFFFF)
+val white40 = Color(0x66FFFFFF)
+val white50 = Color(0x80FFFFFF)
+val white60 = Color(0xA6FFFFFF)
+
+val black1 = gray1
+val black2 = gray2
+val black3 = gray3
+val black4 = gray4
+
+val accountIconLight = Color(0xFFEEEEEE)
+val accountIconDark = Color(0xFF000000)
+val errorRed = Color(0xFFFF3B30)
---
+++ common/src/main/java/jp/co/soramitsu/common/compose/theme/Font.kt
@@ -0,0 +1,13 @@
+package jp.co.soramitsu.common.compose.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import jp.co.soramitsu.common.R
+
+internal val Sora = FontFamily(
+    Font(R.font.soramitsu_extrabold, FontWeight.ExtraBold),
+    Font(R.font.soramitsu_bold, FontWeight.Bold),
+    Font(R.font.soramitsu_semibold, FontWeight.SemiBold),
+    Font(R.font.soramitsu_regular, FontWeight.Normal)
+)
