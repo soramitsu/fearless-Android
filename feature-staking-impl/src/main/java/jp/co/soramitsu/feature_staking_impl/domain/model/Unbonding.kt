6626fa289ff62490e6473c5aec7f7a8d3791d431
Sergey Pankratov <pankratov@soramitsu.co.jp>
2022-08-24 15:43:08 +0500

    fix crash in parcing subquery history

--- feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/domain/model/Unbonding.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/feature_staking_impl/domain/model/Unbonding.kt
@@ -1,12 +1,12 @@
 package jp.co.soramitsu.feature_staking_impl.domain.model
 
+import java.math.BigInteger
+import java.util.Calendar
+import java.util.TimeZone
 import jp.co.soramitsu.common.data.network.subquery.StakingHistoryRemote
 import jp.co.soramitsu.common.utils.orZero
 import jp.co.soramitsu.feature_staking_api.domain.model.DelegationAction
 import jp.co.soramitsu.feature_staking_api.domain.model.DelegationScheduledRequest
-import java.math.BigInteger
-import java.util.Calendar
-import java.util.TimeZone
 
 data class Unbonding(
     val amount: BigInteger,
@@ -24,11 +24,11 @@ fun DelegationScheduledRequest.toUnbonding(timeLeft: Long) = Unbonding(
 
 fun StakingHistoryRemote.HistoryElement.toUnbonding(): Unbonding {
     val timeLeft = this.timestamp?.toLongOrNull()?.let {
-        Calendar.getInstance(TimeZone.getTimeZone("UTC")).get(Calendar.SECOND) - it
+        Calendar.getInstance(TimeZone.getTimeZone("UTC")).timeInMillis / 1000 - it
     } ?: 0
 
     return Unbonding(
-        amount = this.amount.orZero(),
+        amount = this.amount?.toBigInteger().orZero(),
         timeLeft = timeLeft,
         calculatedAt = System.currentTimeMillis(),
         type = DelegationAction.byId(type?.toInt())
