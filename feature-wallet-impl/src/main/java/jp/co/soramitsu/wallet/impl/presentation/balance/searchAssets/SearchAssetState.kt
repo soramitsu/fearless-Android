e1f2b80f7b53f133b028f22f0bf25413c81c3603
Plekhanov <plekhanov@soramitsu.co.jp>
2022-09-27 14:38:29 +0300

    bugfix/FLW-2152-search-assets-fixes

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/balance/searchAssets/SearchAssetState.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/balance/searchAssets/SearchAssetState.kt
@@ -1,8 +1,13 @@
 package jp.co.soramitsu.wallet.impl.presentation.balance.searchAssets
 
+import jp.co.soramitsu.common.compose.component.HiddenItemState
 import jp.co.soramitsu.common.compose.viewstate.AssetListItemViewState
 
 data class SearchAssetState(
     val assets: List<AssetListItemViewState>,
-    val searchQuery: String? = null
-)
+    val searchQuery: String? = null,
+    val hiddenState: HiddenItemState
+) {
+    val visibleAssets = assets.filter { !it.isHidden }
+    val hiddenAssets = assets.filter { it.isHidden }
+}
