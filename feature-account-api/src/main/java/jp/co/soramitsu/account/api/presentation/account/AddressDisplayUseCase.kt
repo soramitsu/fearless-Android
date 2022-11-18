4c8eef84b0a547df1f71d42e1ab203bf198be4ce
Denis Lyazgin <lyazgin@soramitsu.co.jp>
2022-11-18 16:50:25 +0700

    FLW-2200 refactor AddressDisplayUseCase.kt on using meta account

--- feature-account-api/src/main/java/jp/co/soramitsu/account/api/presentation/account/AddressDisplayUseCase.kt
+++ feature-account-api/src/main/java/jp/co/soramitsu/account/api/presentation/account/AddressDisplayUseCase.kt
@@ -1,6 +1,7 @@
 package jp.co.soramitsu.account.api.presentation.account
 
 import jp.co.soramitsu.account.api.domain.interfaces.AccountRepository
+import jp.co.soramitsu.fearless_utils.ss58.SS58Encoder.toAccountId
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -17,7 +18,9 @@ class AddressDisplayUseCase(
     }
 
     suspend operator fun invoke(address: String): String? {
-        return accountRepository.getAccountOrNull(address)?.name
+        return withContext(Dispatchers.Default) {
+            accountRepository.findMetaAccount(address.toAccountId())?.name
+        }
     }
 
     suspend fun createIdentifier(): Identifier = withContext(Dispatchers.Default) {
