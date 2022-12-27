7fd20253cd4970e5f0927a93a9b91a9464067a94
Vitaly Peryatin <peryatin.vitalik37@gmail.com>
2022-12-27 20:38:06 +0300

    Fix paddings in SwapPreviewContent and remove unused TODOs

--- runtime/src/main/java/jp/co/soramitsu/runtime/multiNetwork/ChainRegistry.kt
+++ runtime/src/main/java/jp/co/soramitsu/runtime/multiNetwork/ChainRegistry.kt
@@ -31,7 +31,6 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.launch
 
-// TODO: Move - Minimal
 
 data class ChainService(
     val runtimeProvider: RuntimeProvider,
@@ -42,7 +41,7 @@ class ChainRegistry @Inject constructor(
     private val runtimeProviderPool: RuntimeProviderPool,
     private val connectionPool: ConnectionPool,
     private val runtimeSubscriptionPool: RuntimeSubscriptionPool,
-    private val chainDao: ChainDao, // TODO: Move - no
+    private val chainDao: ChainDao,
     private val chainSyncService: ChainSyncService,
     private val runtimeSyncService: RuntimeSyncService,
     private val updatesMixin: UpdatesMixin
