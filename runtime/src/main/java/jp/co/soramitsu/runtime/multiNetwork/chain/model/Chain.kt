2e94fe43ce6770a93d95969426a9c217bc701d0c
Denis Lyazgin <lyazgin@soramitsu.co.jp>
2023-06-14 16:58:24 +0700

    Add reward calculator for ternoa

--- runtime/src/main/java/jp/co/soramitsu/runtime/multiNetwork/chain/model/Chain.kt
+++ runtime/src/main/java/jp/co/soramitsu/runtime/multiNetwork/chain/model/Chain.kt
@@ -17,6 +17,7 @@ const val rococoChainId = "aaf2cd1b74b5f726895921259421b534124726263982522174147
 const val soraTestChainId = "3266816be9fa51b32cfea58d3e33ca77246bc9618595a4300e44c8856a8d8a17"
 const val soraKusamaChainId = "6d8d9f145c2177fa83512492cdd80a71e29f22473f4a8943a6292149ac319fb9"
 const val soraMainChainId = "7e4e32d0feafd4f9c9414b0be86373f9a1efa904809b683453a9af6856d38ad5"
+const val ternoaChainId = "6859c81ca95ef624c9dfe4dc6e3381c33e5d6509e35e147092bfbc780f777c4e"
 
 const val genshiroChainId = "9b8cefc0eb5c568b527998bdd76c184e2b76ae561be76e4667072230217ea243"
 
