76879b241485630181f61e93d0558d63eb427fcd
Sergey Pankratov <pankratov@soramitsu.co.jp>
2023-07-02 13:28:36 +0500

    FLW-3301 Missing max/25/50/100% percentages on keyboard when entering stake amount. hack for bottomSheet

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/send/setup/SendSetupFragment.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/send/setup/SendSetupFragment.kt
@@ -1,6 +1,7 @@
 package jp.co.soramitsu.wallet.impl.presentation.send.setup
 
 import android.Manifest
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
@@ -9,12 +10,15 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
 import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.journeyapps.barcodescanner.ScanOptions
 import dagger.hilt.android.AndroidEntryPoint
+import java.lang.Integer.max
 import jp.co.soramitsu.common.base.BaseComposeBottomSheetDialogFragment
 import jp.co.soramitsu.common.presentation.ErrorDialog
 import jp.co.soramitsu.common.scan.ScanTextContract
@@ -56,6 +60,29 @@ class SendSetupFragment : BaseComposeBottomSheetDialogFragment<SendSetupViewMode
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        var constantDiff = 0
+        view.postDelayed({
+            val w = Rect()
+            view.getWindowVisibleDisplayFrame(w)
+            constantDiff = view.rootView.height - (w.bottom - w.top)
+        }, 100)
+
+        view.viewTreeObserver.addOnGlobalLayoutListener {
+            val r = Rect()
+            // r will be populated with the coordinates of your view that area still visible.
+            view.getWindowVisibleDisplayFrame(r)
+            val heightDiff: Int = view.rootView.height - (r.bottom - r.top)
+
+            // if more than 100 pixels, its probably a keyboard...
+            viewModel.setSoftKeyboardOpen(heightDiff > 500)
+
+            context?.let {
+                val correctedDiff = max(heightDiff - constantDiff, 0)
+                viewModel.setHeightDiffDp((correctedDiff / Density(it).density).dp)
+            }
+        }
+
         viewModel.openScannerEvent.observeEvent {
             requestCameraPermission()
         }
