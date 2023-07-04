3399748c7d7e2704bf5b1d9d93c27692e129e819
Sergey Pankratov <pankratov@soramitsu.co.jp>
2023-07-04 11:19:46 +0500

    Revert "hide google login"

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/balance/optionswallet/OptionsWalletContent.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/balance/optionswallet/OptionsWalletContent.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import jp.co.soramitsu.common.BuildConfig
 import jp.co.soramitsu.common.compose.component.BottomSheetScreen
 import jp.co.soramitsu.common.compose.component.GrayButton
 import jp.co.soramitsu.common.compose.component.H3
@@ -49,17 +48,15 @@ fun OptionsWalletContent(
         ) {
             H3(text = stringResource(id = R.string.common_title_wallet_option))
             MarginVertical(margin = 28.dp)
-            if (BuildConfig.DEBUG) {
-                GrayButton(
-                    text = stringResource(id = R.string.common_backup_wallet),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(48.dp)
-                ) {
-                    callback.onBackupWalletClick()
-                }
-                MarginVertical(margin = 12.dp)
+            GrayButton(
+                text = stringResource(id = R.string.common_backup_wallet),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+            ) {
+                callback.onBackupWalletClick()
             }
+            MarginVertical(margin = 12.dp)
             GrayButton(
                 text = stringResource(id = R.string.common_details_wallet),
                 modifier = Modifier
