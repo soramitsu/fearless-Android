d312a59fc0fe03e442240f63c06ed8a5692bad16
Vitaly Peryatin <peryatin.vitalik37@gmail.com>
2023-07-24 14:33:10 +0400

    Add fix problem with hilt and buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,4 +16,5 @@ dependencies {
     implementation(libs.gradleplugins.android)
     implementation(libs.gradleplugins.detekt)
     implementation(libs.gradleplugins.kotlin)
+    implementation(libs.gradleplugins.hiltAndroid) // NOTE: hilt wth buildSrc. https://github.com/google/dagger/issues/3068
 }
