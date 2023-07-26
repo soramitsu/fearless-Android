b25f7673d8ccaa2afdb4d31f9822211fd602ec23
Vitaly Peryatin <peryatin.vitalik37@gmail.com>
2023-07-26 14:13:21 +0400

    Add Detekt baseline.xml

--- buildSrc/src/main/kotlin/detekt-setup.gradle.kts
+++ buildSrc/src/main/kotlin/detekt-setup.gradle.kts
@@ -13,43 +13,41 @@ dependencies {
     detektPlugins(libs.findLibrary("detekt-cli").get())
 }
 
-tasks.register<Detekt>("detektAll") {
+tasks.register<Detekt>("detektCheck") {
+    description = "Detekt checking"
     setup(autoCorrect = false)
 }
 
 tasks.register<Detekt>("detektFormat") {
+    description = "Detekt formatting"
     setup(autoCorrect = true)
 }
 
+val detektRulesPath = "$rootDir/android-foundation/src/main/java/jp/co/soramitsu/androidfoundation/detekt/rules.yml"
+
+tasks.register<DetektCreateBaselineTask>("detektGenerateBaseline") {
+    description = "Detekt generating baseline"
+    parallel.set(true)
+    ignoreFailures.set(false)
+    autoCorrect.set(false)
+    buildUponDefaultConfig.set(true)
+    setSource(file(projectDir))
+    baseline.set(file("$rootDir/detekt/baseline.xml"))
+    config.setFrom(files(detektRulesPath))
+    include("**/*.kt")
+    exclude("**/resources/**", "**/build/**")
+}
+
 fun Detekt.setup(autoCorrect: Boolean) {
-    description = "Detekt checking for all modules"
     parallel = true
     ignoreFailures = false
     this.autoCorrect = autoCorrect
     buildUponDefaultConfig = true
     setSource(file(projectDir))
-    config.setFrom(files("$rootDir/detekt/detekt.yml"))
+    baseline.set(file("$rootDir/detekt/baseline.xml"))
+    config.setFrom(files(detektRulesPath))
     include("**/*.kt")
     exclude("**/resources/**", "**/build/**")
-
-    // TODO: Remove exclude paths after merge detekt to develop
-    exclude(
-        "**/common/**",
-        "**/core-api/**",
-        "**/core-db/**",
-        "**/feature-account-api/**", "**/feature-account-impl/**",
-        "**/feature-crowdloan-api/**", "**/feature-crowdloan-impl/**",
-        "**/feature-onboarding-api/**", "**/feature-onboarding-impl/**",
-        "**/feature-polkaswap-api/**", "**/feature-polkaswap-impl/**",
-        "**/feature-soracard-api/**", "**/feature-soracard-impl/**",
-        "**/splash/**",
-        "**/feature-staking-api/**", "**/feature-staking-impl/**",
-        "**/feature-success-api/**", "**/feature-success-impl/**",
-        "**/feature-wallet-api/**", "**/feature-wallet-impl/**",
-        "**/runtime/**", "**/runtime-permission/**",
-        "**/test-shared/**",
-        "**/app/**"
-    )
 }
 
 tasks.withType<Detekt>().configureEach {
