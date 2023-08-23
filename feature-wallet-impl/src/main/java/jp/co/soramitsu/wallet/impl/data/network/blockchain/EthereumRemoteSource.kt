34aca5d819c8239a53923533c2578ed92da00fff
Denis Lyazgin <lyazgin@soramitsu.co.jp>
2023-08-23 14:16:50 +0700

    eth connection pool WIP

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/data/network/blockchain/EthereumRemoteSource.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/data/network/blockchain/EthereumRemoteSource.kt
@@ -1,13 +1,24 @@
 package jp.co.soramitsu.wallet.impl.data.network.blockchain
 
+import android.util.Log
 import java.math.BigInteger
+import jp.co.soramitsu.account.api.domain.model.MetaAccount
+import jp.co.soramitsu.account.api.domain.model.address
+import jp.co.soramitsu.common.data.network.runtime.binding.SimpleBalanceData
+import jp.co.soramitsu.common.utils.failure
 import jp.co.soramitsu.core.models.Asset
 import jp.co.soramitsu.runtime.multiNetwork.chain.model.Chain
+import jp.co.soramitsu.runtime.multiNetwork.chain.model.ChainId
+import jp.co.soramitsu.runtime.multiNetwork.connection.EthereumConnectionPool
 import jp.co.soramitsu.shared_utils.extensions.requireHexPrefix
 import jp.co.soramitsu.shared_utils.extensions.toHexString
 import jp.co.soramitsu.shared_utils.runtime.AccountId
 import jp.co.soramitsu.wallet.impl.domain.model.Transfer
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
 import org.web3j.abi.FunctionEncoder
 import org.web3j.abi.TypeReference
@@ -18,98 +29,231 @@ import org.web3j.abi.datatypes.generated.Uint256
 import org.web3j.crypto.Credentials
 import org.web3j.crypto.RawTransaction
 import org.web3j.crypto.TransactionEncoder
-import org.web3j.protocol.Web3j
+import org.web3j.protocol.Web3jService
 import org.web3j.protocol.core.DefaultBlockParameterName
+import org.web3j.protocol.core.Ethereum
 import org.web3j.protocol.core.Request
 import org.web3j.protocol.core.Response
 import org.web3j.protocol.core.methods.request.Transaction
-import org.web3j.protocol.http.HttpService
 import org.web3j.utils.Convert
 import org.web3j.utils.Numeric
 
-class EthereumRemoteSource {
-
-    suspend fun getTotalBalance(chainAsset: Asset, chain: Chain, accountId: AccountId): BigInteger {
-        val web3 = Web3j.build(HttpService(chain.nodes.first().url))
-        return web3.fetchEthBalance(chainAsset, accountId.toHexString(true))
+class EthereumRemoteSource(private val ethereumConnectionPool: EthereumConnectionPool) {
+    companion object {
+        private const val ETHEREUM_BALANCES_UPDATE_DELAY = 30_000L
     }
 
-    suspend fun performTransfer(chain: Chain, transfer: Transfer, privateKey: String) = withContext(Dispatchers.IO) {
-        val service = HttpService(chain.nodes.first().url)
-        val web3 = Web3j.build(service)
-        val cred = Credentials.create(privateKey)
-        val nonce = web3.ethGetTransactionCount(transfer.sender, DefaultBlockParameterName.LATEST).send().transactionCount
+    suspend fun getTotalBalance(
+        chainAsset: Asset,
+        chain: Chain,
+        accountId: AccountId
+    ): Result<BigInteger> {
+        val connection = ethereumConnectionPool.get(chain.id)
 
-        val amountInPlanks = Convert.toWei(transfer.amount, Convert.Unit.ETHER).toBigInteger()
-
-        val transaction = if (transfer.chainAsset.isUtility) {
-            Transaction.createEtherTransaction(
-                transfer.sender,
-                nonce,
-                null,
-                null,
-                transfer.recipient,
-                amountInPlanks
-            )
-        } else {
-            val function =
-                Function("transfer", listOf(Address(transfer.recipient), Uint256(transfer.amountInPlanks)), listOf(TypeReference.create(Bool::class.java)))
-
-            Transaction.createFunctionCallTransaction(
-                transfer.sender,
-                null,
-                null,
-                null,
-                transfer.chainAsset.id,
-                BigInteger.ZERO,
-                FunctionEncoder.encode(function)
+        return kotlin.runCatching {
+            connection?.web3j!!.fetchEthBalance(
+                chainAsset,
+                accountId.toHexString(true)
             )
         }
+    }
 
-        val baseFeeRaw = web3.ethGetBlockByNumber(DefaultBlockParameterName.LATEST, false).send().resultOrThrow().baseFeePerGas
-        val baseFee = Numeric.decodeQuantity(baseFeeRaw)
-        val priorityFee = service.ethMaxPriorityFeePerGas().send().resultOrThrow().let { Numeric.decodeQuantity(it) }
+    suspend fun performTransfer(
+        chain: Chain,
+        transfer: Transfer,
+        privateKey: String
+    ): Result<String> =
+        withContext(Dispatchers.IO) {
 
-        val estimatedGas = web3.ethEstimateGas(transaction).send().resultOrThrow().let { Numeric.decodeQuantity(it) }
+            val connection = ethereumConnectionPool.get(chain.id)
+                ?: return@withContext Result.failure("There is no connection created for chain ${chain.name}, ${chain.id}")
 
-        val maxFeePerGas = (baseFee + priorityFee) * estimatedGas
-        val chainId = chain.id.requireHexPrefix().drop(2).toLong()
-        val raw = if (transfer.chainAsset.isUtility) {
-            RawTransaction.createEtherTransaction(
-                chainId,
-                nonce,
-                estimatedGas, //gasLimit
-                transfer.recipient,
-                amountInPlanks,
-                priorityFee, //maxPriorityFeePerGas
-                maxFeePerGas //maxFeePerGas
-            )
-        } else {
-            val erc20TransferFunction = Function(
-                "transfer",
-                listOf(Address(transfer.recipient), Uint256(transfer.amountInPlanks)),
-                listOf(TypeReference.create(Bool::class.java))
-            )
+            connection.web3j
+                ?: return@withContext Result.failure("There is no connection established for chain ${chain.name}, ${chain.id}")
+            connection.service
+                ?: return@withContext Result.failure("There is no connection established for chain ${chain.name}, ${chain.id}")
 
-            val encodedErc20Function = FunctionEncoder.encode(erc20TransferFunction)
-            RawTransaction.createTransaction(
-                chainId,
-                nonce,
-                estimatedGas, //gasLimit
-                transfer.chainAsset.id,
-                BigInteger.ZERO,
-                encodedErc20Function,
-                priorityFee, //maxPriorityFeePerGas
-                maxFeePerGas //maxFeePerGas
-            )
+            val web3 = connection.web3j!!
+            val cred = Credentials.create(privateKey)
+            val nonce =
+                kotlin.runCatching {
+                    web3.ethGetTransactionCount(transfer.sender, DefaultBlockParameterName.LATEST)
+                        .send().transactionCount
+                }
+                    .getOrElse { return@withContext Result.failure("Error ethGetTransactionCount for chain ${chain.name}, ${chain.id}, error: $it") }
+
+            val amountInPlanks = Convert.toWei(transfer.amount, Convert.Unit.ETHER).toBigInteger()
+
+            val transaction = if (transfer.chainAsset.isUtility) {
+                Transaction.createEtherTransaction(
+                    transfer.sender,
+                    nonce,
+                    null,
+                    null,
+                    transfer.recipient,
+                    amountInPlanks
+                )
+            } else {
+                val function =
+                    Function(
+                        "transfer",
+                        listOf(Address(transfer.recipient), Uint256(transfer.amountInPlanks)),
+                        listOf(TypeReference.create(Bool::class.java))
+                    )
+
+                Transaction.createFunctionCallTransaction(
+                    transfer.sender,
+                    null,
+                    null,
+                    null,
+                    transfer.chainAsset.id,
+                    BigInteger.ZERO,
+                    FunctionEncoder.encode(function)
+                )
+            }
+
+            val baseFee = runCatching {
+                web3.ethGetBlockByNumber(DefaultBlockParameterName.LATEST, false)
+                    .send()
+                    .resultOrThrow()
+                    .baseFeePerGas
+                    .let { Numeric.decodeQuantity(it) }
+            }
+                .getOrElse { return@withContext Result.failure("Error ethGetBlockByNumber for chain ${chain.name}, ${chain.id}, error: $it") }
+
+            val priorityFee = runCatching {
+                connection.service!!.ethMaxPriorityFeePerGas()
+                    .send()
+                    .resultOrThrow()
+                    .let { Numeric.decodeQuantity(it) }
+            }
+                .getOrElse { return@withContext Result.failure("Error ethMaxPriorityFeePerGas for chain ${chain.name}, ${chain.id}, error: $it") }
+
+            val estimatedGas = kotlin.runCatching {
+                web3.ethEstimateGas(transaction).send()
+                    .resultOrThrow()
+                    .let { Numeric.decodeQuantity(it) }
+            }
+                .getOrElse { return@withContext Result.failure("Error ethEstimateGas for chain ${chain.name}, ${chain.id}, error: $it") }
+
+            val maxFeePerGas = (baseFee + priorityFee) * estimatedGas
+            val chainId = chain.id.requireHexPrefix().drop(2).toLong()
+            val raw = if (transfer.chainAsset.isUtility) {
+                RawTransaction.createEtherTransaction(
+                    chainId,
+                    nonce,
+                    estimatedGas, //gasLimit
+                    transfer.recipient,
+                    amountInPlanks,
+                    priorityFee, //maxPriorityFeePerGas
+                    maxFeePerGas //maxFeePerGas
+                )
+            } else {
+                val erc20TransferFunction = Function(
+                    "transfer",
+                    listOf(Address(transfer.recipient), Uint256(transfer.amountInPlanks)),
+                    listOf(TypeReference.create(Bool::class.java))
+                )
+
+                val encodedErc20Function = FunctionEncoder.encode(erc20TransferFunction)
+                RawTransaction.createTransaction(
+                    chainId,
+                    nonce,
+                    estimatedGas, //gasLimit
+                    transfer.chainAsset.id,
+                    BigInteger.ZERO,
+                    encodedErc20Function,
+                    priorityFee, //maxPriorityFeePerGas
+                    maxFeePerGas //maxFeePerGas
+                )
+            }
+
+            val signed = TransactionEncoder.signMessage(raw, cred)
+
+            val transactionHash = kotlin.runCatching {
+                web3.ethSendRawTransaction(signed.toHexString(true)).send().resultOrThrow()
+            }
+                .getOrElse { return@withContext Result.failure("Error ethSendRawTransaction for chain ${chain.name}, ${chain.id}, error: $it") }
+
+            return@withContext Result.success(transactionHash)
         }
 
-        val signed = TransactionEncoder.signMessage(raw, cred)
+    private val ethereumBalancesSubscriptionJob: MutableMap<ChainId, Job?> = mutableMapOf()
 
-        web3.ethSendRawTransaction(signed.toHexString(true)).send().let {
-            hashCode()
-            it
-        }.resultOrThrow()
+    fun subscribeEthereumBalance(
+        chain: Chain,
+        account: MetaAccount
+    ): Result<Flow<Result<Pair<Asset, SimpleBalanceData>>>> {
+        Log.d("&&&", "subscribeEthereumBalance chain ${chain.name}")
+        val connection = ethereumConnectionPool.get(chain.id)
+        val web3 = connection?.web3j
+            ?: return Result.failure("There is no connection created for chain ${chain.name}, ${chain.id}")
+        Log.d("&&&", "web3 is good on chain ${chain.name}")
+        return Result.success(flow {
+            ethereumBalancesSubscriptionJob[chain.id]?.cancel()
+            ethereumBalancesSubscriptionJob[chain.id] = Job()
+
+            withContext(Dispatchers.Default + ethereumBalancesSubscriptionJob[chain.id]!!) {
+                while (ethereumBalancesSubscriptionJob[chain.id]?.isActive == true) {
+                    val address = account.address(chain)
+                    if (address == null) {
+                        emit(Result.failure("Can't find address for chain ${chain.name} : ${chain.id}, metaAccount: ${account.name}"))
+                        continue
+                    }
+
+                    chain.assets.forEach { asset ->
+                        val balance =
+                            kotlin.runCatching { web3.fetchEthBalance(asset, address) }.getOrElse {
+                                emit(Result.failure("Can't fetchEthBalance for ${asset.name}, ${chain.name}, address: $address"))
+                                return@forEach
+                            }
+                        Log.d("&&&", "got balance for ${asset.name} $balance")
+                        emit(Result.success(asset to SimpleBalanceData(balance)))
+                    }
+                    delay(ETHEREUM_BALANCES_UPDATE_DELAY)
+                }
+            }
+        })
+    }
+
+    suspend fun fetchEthBalance(
+        asset: Asset,
+        address: String): BigInteger {
+        val connection = ethereumConnectionPool.get(asset.chainId)
+        val web3 = connection?.web3j
+            ?: throw RuntimeException("There is no connection created for chain ${asset.chainId}")
+
+        return web3.fetchEthBalance(asset, address)
+    }
+
+    private suspend fun Ethereum.fetchEthBalance(asset: Asset, address: String): BigInteger {
+        return if (asset.isUtility) {
+            withContext(Dispatchers.IO) {
+                ethGetBalance(
+                    address,
+                    DefaultBlockParameterName.LATEST
+                ).send().balance
+            }
+        } else {
+            val erc20GetBalanceFunction = Function(
+                "balanceOf",
+                listOf(Address(address)),
+                emptyList()
+            )
+
+            val erc20BalanceWei = withContext(Dispatchers.IO) {
+                ethCall(
+                    Transaction.createEthCallTransaction(
+                        null,
+                        asset.id,
+                        FunctionEncoder.encode(erc20GetBalanceFunction)
+                    ),
+                    DefaultBlockParameterName.LATEST
+                ).send().value
+            }
+
+            Numeric.decodeQuantity(erc20BalanceWei)
+        }
     }
 }
 
@@ -125,3 +269,18 @@ fun <T> Response<T>.resultOrThrow(): T {
 class EthereumRequestError(message: String) : Exception(message)
 
 class EthereumTransferException(message: String) : Exception(message)
+
+
+fun Web3jService.ethMaxPriorityFeePerGas(): Request<Any, MaxPriorityFeePerGas> {
+    return Request<Any, MaxPriorityFeePerGas>(
+        "eth_maxPriorityFeePerGas",
+        emptyList(),
+        this,
+        MaxPriorityFeePerGas::class.java
+    )
+}
+
+class MaxPriorityFeePerGas : Response<String?>() {
+    val maxPriorityFeePerGas: BigInteger
+        get() = Numeric.decodeQuantity(result)
+}
