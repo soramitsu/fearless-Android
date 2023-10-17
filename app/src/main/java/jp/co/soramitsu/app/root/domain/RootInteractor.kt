8478f03037eb831e7de863984c43636db0081059
Denis Lyazgin <lyazgin@soramitsu.co.jp>
2023-10-17 17:07:42 +0700

    Cashing appConfig, add offline logic for it

--- app/src/main/java/jp/co/soramitsu/app/root/domain/RootInteractor.kt
+++ app/src/main/java/jp/co/soramitsu/app/root/domain/RootInteractor.kt
@@ -1,8 +1,12 @@
 package jp.co.soramitsu.app.root.domain
 
 import jp.co.soramitsu.account.api.domain.PendulumPreInstalledAccountsScenario
+import jp.co.soramitsu.common.data.storage.Preferences
+import jp.co.soramitsu.common.data.storage.appConfig
+import jp.co.soramitsu.common.domain.model.AppConfig
 import jp.co.soramitsu.common.domain.model.toDomain
 import jp.co.soramitsu.common.utils.inBackground
+import jp.co.soramitsu.common.utils.requireValue
 import jp.co.soramitsu.core.updater.UpdateSystem
 import jp.co.soramitsu.core.updater.Updater
 import jp.co.soramitsu.wallet.impl.data.buyToken.ExternalProvider
@@ -13,7 +17,8 @@ import kotlinx.coroutines.flow.flowOf
 class RootInteractor(
     private val updateSystem: UpdateSystem,
     private val walletRepository: WalletRepository,
-    private val pendulumPreInstalledAccountsScenario: PendulumPreInstalledAccountsScenario
+    private val pendulumPreInstalledAccountsScenario: PendulumPreInstalledAccountsScenario,
+    private val preferences: Preferences
 ) {
 
     fun runBalancesUpdate(): Flow<Updater.SideEffect> = updateSystem.start().inBackground()
@@ -28,7 +33,17 @@ class RootInteractor(
         }
     }
 
-    suspend fun getRemoteConfig() = walletRepository.getRemoteConfig().map { it.toDomain() }
+    suspend fun getRemoteConfig(): Result<AppConfig> {
+        val remoteVersion = walletRepository.getRemoteConfig()
+
+        return if (remoteVersion.isSuccess) {
+            preferences.appConfig = remoteVersion.requireValue()
+            remoteVersion
+        } else {
+            val localVersion = preferences.appConfig
+            Result.success(localVersion)
+        }.map { it.toDomain() }
+    }
 
     fun chainRegistrySyncUp() = walletRepository.chainRegistrySyncUp()
 
