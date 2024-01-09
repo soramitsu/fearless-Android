5d0c40acec7d0b37ccb04a9feed11cc74927c064
Sergey Pankratov <pankratov@soramitsu.co.jp>
2024-01-09 18:01:03 +0500

    FLW-3884 Create contact. Button is under the keyboard; autofocus

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/addressbook/CreateContactContent.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/addressbook/CreateContactContent.kt
@@ -8,8 +8,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,6 +56,9 @@ fun CreateContactContent(
     state: CreateContactViewState,
     callback: CreateContactScreenInterface
 ) {
+    val focusRequester = remember { FocusRequester() }
+    LaunchedEffect(Unit) { focusRequester.requestFocus() }
+
     BottomSheetScreen {
         Box(Modifier.fillMaxSize()) {
             Column(
@@ -70,6 +77,7 @@ fun CreateContactContent(
                 )
                 MarginVertical(margin = 12.dp)
                 InputWithHintCornered(
+                    inputModifier = Modifier.focusRequester(focusRequester),
                     input = state.contactNameInput,
                     hint = stringResource(id = R.string.contact_name),
                     onInput = callback::onNameInput
@@ -99,6 +107,7 @@ fun CreateContactContent(
 
 @Composable
 fun InputWithHintCornered(
+    inputModifier: Modifier = Modifier,
     input: String?,
     hint: String?,
     onInput: (String) -> Unit
@@ -113,7 +122,7 @@ fun InputWithHintCornered(
         InputWithHint(
             state = input,
             onInput = onInput,
-            modifier = Modifier
+            modifier = inputModifier
                 .fillMaxSize()
                 .align(Alignment.CenterStart)
                 .padding(horizontal = 12.dp)
