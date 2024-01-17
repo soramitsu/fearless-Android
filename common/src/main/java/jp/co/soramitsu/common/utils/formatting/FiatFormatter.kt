c0c0ba4fa79d28d6024d05e9d252d9601c51ccff
Sergey Pankratov <pankratov@soramitsu.co.jp>
2024-01-17 22:29:04 +0500

    FLW-4263 Fix fiat decimals on the main assets list and details

--- common/src/main/java/jp/co/soramitsu/common/utils/formatting/FiatFormatter.kt
+++ common/src/main/java/jp/co/soramitsu/common/utils/formatting/FiatFormatter.kt
@@ -2,16 +2,40 @@ package jp.co.soramitsu.common.utils.formatting
 
 import jp.co.soramitsu.common.utils.decimalFormatterFor
 import java.math.BigDecimal
+import java.text.DecimalFormatSymbols
+import java.util.Locale
 
-class FiatFormatter : NumberFormatter {
+class FiatFormatter(
+    private val locale: Locale? = null
+) : NumberFormatter {
     companion object {
         private const val FIAT_DECIMAL_PATTERN = "#,##0.00"
     }
 
     override fun format(number: BigDecimal): String {
         val delegate = decimalFormatterFor(FIAT_DECIMAL_PATTERN)
+        locale?.let {
+            delegate.decimalFormatSymbols = DecimalFormatSymbols(locale)
+        }
         val decimalSeparator = delegate.decimalFormatSymbols.decimalSeparator
         val leadingZeros = Regex(decimalSeparator + "00$")
         return delegate.format(number).replace(leadingZeros, "")
+    }
+}
+
+class FiatSmallFormatter(
+    private val locale: Locale? = null
+) : NumberFormatter {
+    companion object {
+        private const val FIAT_DECIMAL_PATTERN = "#.###E0"
+    }
+
+    override fun format(number: BigDecimal): String {
+        val delegate = decimalFormatterFor(FIAT_DECIMAL_PATTERN)
+        locale?.let {
+            delegate.decimalFormatSymbols = DecimalFormatSymbols(locale)
+        }
+        val formattedValue = delegate.format(number).toBigDecimal()
+        return FullPrecisionFormatter(locale).format(formattedValue)
     }
 }
--- common/src/test/java/jp/co/soramitsu/common/utils/formatting/FiatFormatterTest.kt
+++ common/src/test/java/jp/co/soramitsu/common/utils/formatting/FiatFormatterTest.kt
@@ -1,11 +1,24 @@
 package jp.co.soramitsu.common.utils.formatting
 
+import java.util.Locale
 import org.junit.Test
 
 class FiatFormatterTest {
 
-    private val formatter = FiatFormatter()
+    private val formatter = FiatFormatter(Locale.US)
+    private val formatterSmall = FiatSmallFormatter(Locale.US)
 
+    @Test
+    fun `test small fiat format`() {
+        testFormatter(formatterSmall, "0.001234", "0.0012345678")
+        testFormatter(formatterSmall, "0.0001234", "0.00012345678")
+        testFormatter(formatterSmall, "0.000000000001004", "0.0000000000010045678")
+        testFormatter(formatterSmall, "0.06", "0.06")
+        testFormatter(formatterSmall, "0.067", "0.067")
+        testFormatter(formatterSmall, "0.123", "0.123")
+        testFormatter(formatterSmall, "120.1", "120.123")
+        testFormatter(formatterSmall, "120,300,000", "120340034.123")
+    }
     @Test
     fun `test format`() {
         testFormatter(formatter, "1.23", "1.2345")
