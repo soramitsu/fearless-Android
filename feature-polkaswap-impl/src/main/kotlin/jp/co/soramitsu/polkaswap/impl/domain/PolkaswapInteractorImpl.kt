deb27092fe28e569a22f124e4633d54aa28de615
Sergey Pankratov <pankratov@soramitsu.co.jp>
2024-02-22 15:21:00 +0500

    FLW-4394 crash fix: crash on liquidity request error

--- feature-polkaswap-impl/src/main/kotlin/jp/co/soramitsu/polkaswap/impl/domain/PolkaswapInteractorImpl.kt
+++ feature-polkaswap-impl/src/main/kotlin/jp/co/soramitsu/polkaswap/impl/domain/PolkaswapInteractorImpl.kt
@@ -243,12 +243,9 @@ class PolkaswapInteractorImpl @Inject constructor(
         val tokenFromId = requireNotNull(tokenInput.token.configuration.currencyId)
         val tokenToId = requireNotNull(tokenOutput.token.configuration.currencyId)
 
-        val sources = try {
-            polkaswapRepository.getAvailableSources(polkaswapChainId, tokenFromId, tokenToId, availableDexes)
-                .mapValues { listOf(Market.SMART, *it.value.toTypedArray()) }
-        } catch (e: Exception) {
-            availableDexes.associateWith { listOf(Market.SMART) }
-        }
+        val sources = polkaswapRepository.getAvailableSources(polkaswapChainId, tokenFromId, tokenToId, availableDexes)
+            .mapValues { listOf(Market.SMART, *it.value.toTypedArray()) }
+
         availableMarkets.clear()
         availableMarkets.putAll(sources)
         return sources.values.flatten().toSet()
