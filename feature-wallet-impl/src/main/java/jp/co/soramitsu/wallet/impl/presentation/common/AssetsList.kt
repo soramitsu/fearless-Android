efec9d0fc82c44251ecae6c30b6ed84eef595c81
Sergey Pankratov <pankratov@soramitsu.co.jp>
2024-04-10 22:07:31 +0500

    FLW-4525 Hide all assets. We should show alert and sumimasen text

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/common/AssetsList.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/common/AssetsList.kt
@@ -2,8 +2,8 @@ package jp.co.soramitsu.wallet.impl.presentation.common
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -15,12 +15,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import jp.co.soramitsu.common.compose.component.ActionItemType
 import jp.co.soramitsu.common.compose.component.B0
+import jp.co.soramitsu.common.compose.component.GradientIcon
+import jp.co.soramitsu.common.compose.component.H3
 import jp.co.soramitsu.common.compose.component.MarginVertical
 import jp.co.soramitsu.common.compose.component.SwipeState
+import jp.co.soramitsu.common.compose.theme.alertYellow
+import jp.co.soramitsu.common.compose.theme.white50
 import jp.co.soramitsu.common.compose.viewstate.AssetListItemViewState
 import jp.co.soramitsu.feature_wallet_impl.R
 import jp.co.soramitsu.runtime.multiNetwork.chain.model.ChainId
@@ -40,29 +43,28 @@ fun AssetsList(
     header: (@Composable () -> Unit)? = null,
     footer: (@Composable () -> Unit)? = null
 ) {
-    LazyColumn(
-        state = listState,
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        contentPadding = PaddingValues(top = 8.dp)
-    ) {
-        if (header != null) {
-            item { header() }
-        }
-        if (data.assets.isEmpty()) {
-            item {
-                Box(
-                    modifier = Modifier
-                        .defaultMinSize(minHeight = 48.dp)
-                        .fillMaxWidth(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    B0(
-                        text = stringResource(id = R.string.wallet_all_assets_hidden),
-                        textAlign = TextAlign.Center
-                    )
-                }
+    if (data.assets.isEmpty()) {
+        Column {
+            MarginVertical(margin = 8.dp)
+            header?.invoke()
+            Box(
+                modifier = Modifier.weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                EmptyAssetsContent()
             }
-        } else {
+            footer?.invoke()
+            MarginVertical(margin = 80.dp)
+        }
+    } else {
+        LazyColumn(
+            state = listState,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(top = 8.dp)
+        ) {
+            if (header != null) {
+                item { header() }
+            }
             items(data.assets, key = { it.key }) { assetState ->
                 SwipeableAssetListItem(
                     assetState = assetState,
@@ -70,10 +72,32 @@ fun AssetsList(
                     actionItemClicked = callback::actionItemClicked
                 )
             }
+            if (footer != null) {
+                item { footer() }
+            }
+            item { MarginVertical(margin = 80.dp) }
         }
-        if (footer != null) {
-            item { footer() }
-        }
-        item { MarginVertical(margin = 80.dp) }
+    }
+}
+
+@Composable
+fun EmptyAssetsContent() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        GradientIcon(
+            iconRes = R.drawable.ic_alert_24,
+            color = alertYellow,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            contentPadding = PaddingValues(bottom = 4.dp)
+        )
+
+        H3(text = stringResource(id = R.string.common_search_assets_alert_title))
+        B0(
+            text = stringResource(id = R.string.wallet_all_assets_hidden),
+            color = white50
+        )
     }
 }
