ab2a517be6c206ace389ac8ff0570ff5509c096b
Sergey Pankratov <pankratov@soramitsu.co.jp>
2024-12-10 21:16:40 +0500

    FLW-5114 TON wallet. There shouldn’t be Wallet Details on the Settings

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/balance/optionswallet/OptionsWalletViewModel.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/balance/optionswallet/OptionsWalletViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import jp.co.soramitsu.account.api.domain.interfaces.AccountInteractor
 import jp.co.soramitsu.common.base.BaseViewModel
 import jp.co.soramitsu.common.utils.Event
@@ -14,10 +15,10 @@ import jp.co.soramitsu.wallet.impl.presentation.WalletRouter
 import jp.co.soramitsu.wallet.impl.presentation.balance.optionswallet.OptionsWalletFragment.Companion.KEY_WALLET_ID
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @HiltViewModel
 class OptionsWalletViewModel @Inject constructor(
@@ -30,21 +31,30 @@ class OptionsWalletViewModel @Inject constructor(
     private val _deleteWalletConfirmation = MutableLiveData<Event<Long>>()
     val deleteWalletConfirmation: LiveData<Event<Long>> = _deleteWalletConfirmation
 
-    private val selectedWallet = accountInteractor.selectedMetaAccountFlow()
+    private val selectedWallet = accountInteractor.selectedLightMetaAccountFlow()
         .inBackground()
         .share()
 
-    private val nameFlow = flowOf {
-        accountInteractor.getMetaAccount(walletId).name
-    }.stateIn(viewModelScope, SharingStarted.Eagerly, null)
+    private val chosenWallet = flowOf { accountInteractor.getMetaAccount(walletId) }
+        .inBackground()
+        .share()
 
-    val state: StateFlow<OptionsWalletScreenViewState> = selectedWallet.map {
+    private val nameFlow = chosenWallet.map { it.name }
+        .stateIn(viewModelScope, SharingStarted.Eagerly, null)
 
-        OptionsWalletScreenViewState(it.id == walletId, it.ethereumAddress != null)
+    val state: StateFlow<OptionsWalletScreenViewState> = combine(
+        selectedWallet,
+        chosenWallet
+    ) { selectedWallet, chosenWallet ->
+        OptionsWalletScreenViewState(
+            isSelected = selectedWallet.id == walletId,
+            showScoreButton = chosenWallet.ethereumAddress != null,
+            showDetailsButton = chosenWallet.tonPublicKey == null
+        )
     }.stateIn(
         viewModelScope,
         SharingStarted.Eagerly,
-        OptionsWalletScreenViewState(isSelected = true, showScoreButton = false)
+        OptionsWalletScreenViewState(isSelected = true, showScoreButton = false, showDetailsButton = false)
     )
 
     override fun onChangeWalletNameClick() {
