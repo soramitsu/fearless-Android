ebeb0f3f2c83f32803d03fe54b56591b8fcf8a0a
Sergey Pankratov <pankratov@soramitsu.co.jp>
2025-01-14 12:45:17 +0500

    fixes/release (#1214)
    * fix input background
    
    * version code up
    
    * nothing to claim warning
    
    * 207
    
    * fixed MAX amount button in staking, improve debugging by breakpoints
    
    Signed-off-by: Deneath <lyazgindenis@gmail.com>
    
    * strings update
    
    * stop chosen node from reset
    
    * 208
    
    * toc-connect accept urls
    
    * build up 209
    
    ---------
    
    Signed-off-by: Deneath <lyazgindenis@gmail.com>
    Co-authored-by: Deneath <lyazgindenis@gmail.com>

--- core-db/src/main/java/jp/co/soramitsu/coredb/model/chain/ChainNodeLocal.kt
+++ core-db/src/main/java/jp/co/soramitsu/coredb/model/chain/ChainNodeLocal.kt
@@ -25,4 +25,26 @@ data class ChainNodeLocal(
     val name: String,
     val isActive: Boolean,
     val isDefault: Boolean
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ChainNodeLocal
+
+        if (chainId != other.chainId) return false
+        if (url != other.url) return false
+        if (name != other.name) return false
+        if (isDefault != other.isDefault) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = chainId.hashCode()
+        result = 31 * result + url.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + isDefault.hashCode()
+        return result
+    }
+}
