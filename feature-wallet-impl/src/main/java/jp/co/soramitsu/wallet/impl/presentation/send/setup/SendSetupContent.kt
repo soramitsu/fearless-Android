ec4ac40dbe542aa7c65f18c1662d79b994dfbf67
Sergey Pankratov <pankratov@soramitsu.co.jp>
2025-01-17 16:51:21 +0500

    FLW-4958 [Android] [BUG] Send. There is two screen of warning

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/send/setup/SendSetupContent.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/wallet/impl/presentation/send/setup/SendSetupContent.kt
@@ -4,7 +4,6 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
