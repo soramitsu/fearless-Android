a387a541a91286153781692122321939dbd3182f
William Richter <richter@soramitsu.co.jp>
2026-01-27 19:15:10 +0100

    fix(staking): revisit active era flow mapping

--- feature-staking-impl/src/main/java/jp/co/soramitsu/staking/impl/data/network/blockhain/updaters/Common.kt
+++ feature-staking-impl/src/main/java/jp/co/soramitsu/staking/impl/data/network/blockhain/updaters/Common.kt
@@ -14,14 +14,16 @@ import jp.co.soramitsu.shared_utils.wsrpc.SocketService
 import jp.co.soramitsu.staking.impl.data.network.blockhain.bindings.bindActiveEra
 import java.math.BigInteger
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.flow.map
 
 fun RuntimeMetadata.activeEraStorageKeyOrNull() = moduleOrNull(Modules.STAKING)?.storage("ActiveEra")?.storageKey()
 fun RuntimeMetadata.stakingRoundKey() = parachainStaking().storage("Round").storageKey()
 
 suspend fun StorageCache.observeActiveEraIndex(runtime: RuntimeSnapshot, chainId: String): Flow<BigInteger> {
     return observeEntry(runtime.metadata.activeEraStorageKeyOrNull(), chainId)
-        .mapNotNull { entry -> entry.content?.let { bindActiveEra(it, runtime) } }
+        .map { entry ->
+            entry.content?.let { bindActiveEra(it, runtime) }
+        }
 }
 
 suspend fun BulkRetriever.fetchValuesToCache(
