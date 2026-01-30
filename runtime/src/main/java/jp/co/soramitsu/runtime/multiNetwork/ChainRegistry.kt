e7ac24539b092d8a603cedcc412ecd53d685b6e6
William Richter <88400283+WRRicht3r@users.noreply.github.com>
2026-01-30 09:45:50 +0100

    fix: release build fixes (#1246)
    * fix: release build fixes
    
    * fix: avoid pausing updated chains

--- runtime/src/main/java/jp/co/soramitsu/runtime/multiNetwork/ChainRegistry.kt
+++ runtime/src/main/java/jp/co/soramitsu/runtime/multiNetwork/ChainRegistry.kt
@@ -127,7 +127,10 @@ class ChainRegistry @Inject constructor(
     fun syncUp() {
         chainsToSync.onEach { (removed, addedOrModified, all) ->
             coroutineScope {
-                val removedDeferred = removed.map {
+                val addedIds = addedOrModified.map(Chain::id).toSet()
+                val removedTrue = removed.filterNot { it.id in addedIds } // skip chains that are just updated
+
+                val removedDeferred = removedTrue.map {
                     async { connectionPool.getConnectionOrNull(it.id)?.socketService?.pause() }
                 }
 
